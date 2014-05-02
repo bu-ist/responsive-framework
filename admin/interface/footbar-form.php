8484ca739c70b8140b3cefcca83d5ba295e908e7
Steve Rudolfi <srudolfi@bu.edu>
2014-05-02 15:19:08 +0000

    clearing house from old version based on flexi

--- admin/interface/footbar-form.php
+++
@@ -1,8 +0,0 @@
-<fieldset>
-<?php foreach( $footbars as $id => $footbar): ?>
-	<p><input type="radio" id="footbar_layout_<?php echo $id; ?>" name="footbar_layout" value="<?php echo $id; ?>" <?php checked( $id, $selected ); ?>/>
-	<label for="footbar_layout_<?php echo $id; ?>"><?php echo $footbar['name']; ?></label></p>
-<?php endforeach; ?>
-	<p><input type="radio" id="footbar_layout_none" name="footbar_layout" value="none" <?php checked('none', $selected ); ?>/>
-	<label for="footbar_layout_none">None</label></p>
-</fieldset>
--- sidebar-footbar.php
+++
@@ -1,8 +0,0 @@
-<?php $footbar = bu_flexi_get_footbar_for_post( get_the_ID() ); ?>
-<?php if(is_active_sidebar($footbar)) : ?>
-<aside id="footbar1"<?php bu_flexi_footbar_class( $footbar ); ?> role="complementary">
-	<div class="container">
-	<?php dynamic_sidebar($footbar); ?>
-	</div><!-- /.container -->
-</aside><!-- /#footbar -->
-<?php endif; ?>
