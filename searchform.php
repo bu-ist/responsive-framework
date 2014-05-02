05ffcff51a720e1481c195816ff0bdd6a6cd8966
Steve Rudolfi <srudolfi@bu.edu>
2014-05-02 15:19:08 +0000

    clearing house from old version based on flexi

--- searchform.php
+++
@@ -1,7 +0,0 @@
-<form role="search" id="quicksearch" action="<?php echo home_url( '/' ); ?>" method="get">
-	<fieldset>
-		<label class="screen-reader-text" for="s"><?php __('Search for:'); ?></label>
-		<input type="search" alt="Search" name="s" value="<?php get_search_query(); ?>" />
-		<button type="submit" id="searchsubmit"  value="<?php esc_attr__('Search'); ?>" />
-	</fieldset>
-</form>
