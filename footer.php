b20607dd76c0135e2fb77e00e30de38d38b4fc68
Steve Rudolfi <srudolfi@bu.edu>
2014-05-06 16:09:38 +0000

    initial barebones-based theme commit

---
+++ footer.php
@@ -0,0 +1,11 @@
+<?php get_sidebar(); ?>
+
+<footer role="contentinfo">
+	<p>&copy;<?php echo date("Y"); ?> <a href="#top" title="Jump back to top">&#8593;</a></p>
+</footer>
+
+<?php wp_footer(); ?>
+
+
+</body>
+</html>
