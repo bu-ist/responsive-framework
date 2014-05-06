02ac5e47855432e90c363501330cc87afde644b1
Steve Rudolfi <srudolfi@bu.edu>
2014-05-06 16:09:38 +0000

    initial barebones-based theme commit

---
+++ sidebar.php
@@ -0,0 +1,6 @@
+<?php ?>
+<section role="complementary" class="secondary-content">
+	<?php if ( !function_exists('dynamic_sidebar')
+        || !dynamic_sidebar() ) : ?>
+	<?php endif; ?>
+</section>
