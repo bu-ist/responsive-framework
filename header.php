b72b93bf924583dd3220c6b45c0ce66cb0a172a5
Steve Rudolfi <srudolfi@bu.edu>
2014-05-09 20:31:50 +0000

    responsive-framework: parting out the fonts, some header styles

--- header.php
+++ header.php
@@ -1,61 +1,60 @@
-<?php ?>
 <!DOCTYPE html>
 <html>
-<head>
-<title><?php
-global $page, $paged;
-wp_title( '|', true, 'right' );
-bloginfo( 'name' );
-$site_description = get_bloginfo( 'description', 'display' );
-if ( $site_description && ( is_home() || is_front_page() ) )
-echo " | $site_description";
-if ( $paged >= 2 || $page >= 2 )
-echo ' | ' . sprintf( __( 'Page %s' ), max( $paged, $page ) );
-?>
-</title>
-<meta name="description" content="<?php if ( is_single() ) {
-single_post_title('', true);
-} else {
-bloginfo('name'); echo " - "; bloginfo('description');
-}
-?>" />
-<meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta name="viewport" content="initial-scale=1" />
-<meta http-equiv="ClearType" content="true" />
+	<head>
+		<title><?php
+			global $page, $paged;
+			wp_title( '|', true, 'right' );
+			bloginfo( 'name' );
+			$site_description = get_bloginfo( 'description', 'display' );
+			if ( $site_description && ( is_home() || is_front_page() ) )
+				echo " | $site_description";
+			if ( $paged >= 2 || $page >= 2 )
+				echo ' | ' . sprintf( __( 'Page %s' ), max( $paged, $page ) );
+			?>
+		</title>
+		
+		<meta name="description" content="<?php if ( is_single() ) {
+			single_post_title('', true);
+			} else {
+				bloginfo('name'); echo " - "; bloginfo('description');
+			}
+		?>" />
+		
+		<meta charset="<?php bloginfo( 'charset' ); ?>" />
+		<meta name="viewport" content="initial-scale=1" />
+		<meta http-equiv="ClearType" content="true" />
 
-<!-- The little things -->
-<link rel="profile" href="http://gmpg.org/xfn/11" />
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-<link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/favicon.ico">
-<link rel="apple-touch-icon" href="<?php echo bloginfo('template_directory'); ?>/apple-touch-icon-precomposed.png"/>
-<!-- The little things -->
 
-<!-- Stylesheets -->
-<link rel="stylesheet" type="text/css" href="//cloud.typography.com/6127692/660644/css/fonts.css" />
-<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
-<!-- Stylesheets -->
+		<link rel="profile" href="http://gmpg.org/xfn/11" />
+		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+		<link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/favicon.ico">
+		<link rel="apple-touch-icon" href="<?php echo bloginfo('template_directory'); ?>/apple-touch-icon-precomposed.png"/>
 
-<!-- Load scripts quick smart -->
 
-<!-- Load scripts quick smart -->
+		<!-- Stylesheets -->
+		<link rel="stylesheet" type="text/css" href="//cloud.typography.com/6127692/660644/css/fonts.css" />
+		<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+		<!-- Stylesheets -->
 
-<?php wp_deregister_script('jquery');wp_head(); ?>
-</head>
 
-<body <?php body_class(); ?> id="top">
-<header role="banner">
-<a id="siteName" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home" class="logo">Boston University <span><?php bloginfo( 'name' ); ?></span></a>
-<p class="desc">
-<?php bloginfo( 'description' ); ?>
-</p>
-<nav role="navigation">
-<?php
+
+		<?php wp_deregister_script('jquery');wp_head(); ?>
+	</head>
+
+	<body <?php body_class(); ?> id="top">
+		<header role="banner">
+			<a id="siteName" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home" class="logo">
+				Boston University <span><?php bloginfo( 'name' ); ?></span>
+			</a>
+			<p class="desc"><?php bloginfo( 'description' ); ?></p>
+			<nav role="navigation">
+				<?php
                 $args = array(
                     'container' => 'false',
                     'items_wrap' => '<ul>%3$s</ul>',
                     );
                 wp_nav_menu($args);
-            ?>
-</nav>
-<?php get_search_form(); ?>
-</header>
+				?>
+			</nav>
+			<?php get_search_form(); ?>
+		</header>
