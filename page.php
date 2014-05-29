dbe0ca522849cb5c6aa990acea2678d30f658f7f
Jon Brousseau <jonpb@bu.edu>
2014-05-29 18:50:07 +0000

    applying bootstrap grid to responsi

--- page.php
+++ page.php
@@ -1,24 +1,23 @@
 <?php get_header(); ?>
 
 	<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-		
-	    <article role="main" class="primary-content" id="post-<?php the_ID(); ?>">
-	    	<?php if (function_exists('bu_content_banner')) {
-				bu_content_banner($post->ID, $args = array(
-					'before' => '<div class="banner-container">',
-					'after' => '</div>',
-					'class' => 'banner',
-					//'maxwidth' => 900,
-					'position' => 'content-width'
-					));
-			} ?>
 	    
+    	<?php if (function_exists('bu_content_banner')) {
+			bu_content_banner($post->ID, $args = array(
+				'before' => '<div class="banner-container">',
+				'after' => '</div>',
+				'class' => 'banner',
+				//'maxwidth' => 900,
+				'position' => 'content-width'
+				));
+		} ?>
+			
+		<article role="main" class="col-md-9" id="post-<?php the_ID(); ?>">
 	        <?php if ( is_front_page() ) { ?>
 	            <h1><?php the_title(); ?></h1>
 	        <?php } else { ?>
 	            <h1><?php the_title(); ?></h1>
 	        <?php } ?>
-	    
 	        <?php the_content(); ?>
 	        
 			<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:' ), 'after' => '</div>' ) ); ?>
@@ -28,15 +27,16 @@
 	    
 	        <?php endwhile; ?>
 	    </article>
+	    
 	    <?php
 	    	if(is_dynamic_sidebar("right-content-area")):
 				?>
-				<aside id="right-content-area">
+				<aside class="col-md-3" id="right-content-area">
 					<?php dynamic_sidebar("right-content-area"); ?>
 				</aside>
 				<?php
-	    	endif;
-	    	
+	    	endif;	    	
 	    ?>
+	    
 	</div>
 <?php get_footer( 'no-sidebar' ); // will include footer-no-sidebar.php; ?>
