c75639f623ddd27ba486f0435c9341784a824633
Steve Rudolfi <srudolfi@bu.edu>
2014-06-19 19:47:32 +0000

    removing searchform

--- searchform.php
+++
@@ -1,7 +0,0 @@
-<form id="quicksearch" action="/" method="get">
-    <fieldset>
-        <label for="search">Search in <?php echo home_url( '/' ); ?></label>
-        <input type="text" name="s" id="search" value="<?php the_search_query(); ?>" />
-        <input type="submit" value="Search" name="do_search" class="button">
-    </fieldset>
-</form>
