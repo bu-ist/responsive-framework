c4733a1b9354a1d9ab5a23725e1de673388363ae
Jon Brousseau <jonpb@bu.edu>
2014-06-25 19:49:35 +0000

    responsi: numerous changes, edits, theme editions. cake will be served.

--- page.php
+++ page.php
@@ -1,18 +1,28 @@
 <?php get_header(); ?>
 	<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-	    
-    	<?php if (function_exists('bu_content_banner')) {
+		<?php if (function_exists('bu_content_banner')) {
 			echo(do_shortcode(bu_content_banner($post->ID, $args = array(
-				'before' => '<div class="banner-container">',
+				'before' => '<div class="banner-container page-width">',
 				'after' => '</div>',
 				'class' => 'banner',
 				//'maxwidth' => 900,
-				'position' => 'content-width',
+				'position' => 'page-width',
 				'echo' => false
 				))));
-		} ?>
-			
+		} ?>	
+		
 		<article role="main" class="col-md-8" id="post-<?php the_ID(); ?>">
+			<?php if (function_exists('bu_content_banner')) {
+				echo(do_shortcode(bu_content_banner($post->ID, $args = array(
+					'before' => '<div class="banner-container content-width">',
+					'after' => '</div>',
+					'class' => 'banner',
+					//'maxwidth' => 900,
+					'position' => 'content-width',
+					'echo' => false
+					))));
+			} ?>
+		
 	        <?php if ( is_front_page() ) { ?>
 	            <h1><?php the_title(); ?></h1>
 	        <?php } else { ?>
