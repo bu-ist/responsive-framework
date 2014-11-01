176f14f3f96174d65b60f6448cb8dd94d1593b81
Mike Burns <mgburns@bu.edu>
2014-10-31 23:19:53 -0400

    Add shortcodes.php.

---
+++ inc/shortcodes.php
@@ -0,0 +1,22 @@
+<?php
+
+function buniverse_video_func( $atts ) {
+	$atts = shortcode_atts( array(
+			'vid'     => '',
+			'id'      => '',
+			'class'   => '',
+			'caption' => '',
+		), $atts );
+
+	$retstr = '<div class="vid"> <div id="'. $atts['id'] . '" class="responsive-video ' . $atts['class'] . '"><div>';
+	$retstr .= '<iframe width="550" height="310" frameborder="0" src="http://www.bu.edu/buniverse/interface/embed/embed.html?v=' . $atts['vid'] . '"></iframe>';
+	$retstr .= '</div></div>';
+	if ( $atts['caption'] ) {
+		$retstr .= '<p class="caption">' . $atts['caption'] . '</p>';
+	}
+	$retstr .= '</div>';
+
+	return $retstr;
+}
+
+add_shortcode( 'buniverse', 'buniverse_video_func' );
