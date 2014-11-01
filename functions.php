a2472d1a96ad475491c1f2ef35c23e0d42f836cd
Mike Burns <mgburns@bu.edu>
2014-10-31 23:19:53 -0400

    Add shortcodes.php.

--- functions.php
+++ functions.php
@@ -72,27 +72,6 @@ if ( class_exists( 'AllowedTemplates' ) ) {
 	$profile_templates->register( array( 'profiles.php' ) );
 }
 
-function buniverse_video_func( $atts ) {
-	$atts = shortcode_atts( array(
-			'vid'     => '',
-			'id'      => '',
-			'class'   => '',
-			'caption' => '',
-		), $atts );
-
-	$retstr = '<div class="vid"> <div id="'. $atts['id'] . '" class="responsive-video ' . $atts['class'] . '"><div>';
-	$retstr .= '<iframe width="550" height="310" frameborder="0" src="http://www.bu.edu/buniverse/interface/embed/embed.html?v=' . $atts['vid'] . '"></iframe>';
-	$retstr .= '</div></div>';
-	if ( $atts['caption'] ) {
-		$retstr .= '<p class="caption">' . $atts['caption'] . '</p>';
-	}
-	$retstr .= '</div>';
-
-	return $retstr;
-}
-
-add_shortcode( 'buniverse', 'buniverse_video_func' );
-
 /* - - - - - - - - - - - - - - - - -
   Register All Scripts and Styles
   - - - - - - - - - - - - - - - - - */
@@ -234,6 +213,11 @@ require __DIR__ . '/inc/post-lists.php';
  * @link http://bifrost.bu.edu/svn/repos/wordpress/plugins/bu-profiles
  */
 require __DIR__ . '/inc/profiles.php';
+
+/**
+ * Shortcodes for content editors.
+ */
+require __DIR__ . '/inc/shortcodes.php';
 
 /**
  * Reusable template tags to keep templates logic-free.
