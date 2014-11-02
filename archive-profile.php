659a7f48478667a6e378ca9551032358bf30dd6c
Mike Burns <mgburns@bu.edu>
2014-11-01 20:22:44 -0400

    Fix validation errors.

--- archive-profile.php
+++ archive-profile.php
@@ -2,8 +2,10 @@
 
 	<article role="main" class="col-md-8" id="post-<?php the_ID(); ?>">
 
-	<h1>Profile Directory</h1>
-	<?php bu_profile_get_template_part( 'basic' ); ?>
-	<p class="navigation"><span class="previous"><?php previous_posts_link( 'Previous' ) ?></span> <span class="next"><?php next_posts_link( 'Next' ) ?></span></p><!-- /.navigation -->
+		<h1>Profile Directory</h1>
+		<?php bu_profile_get_template_part( 'basic' ); ?>
+		<p class="navigation"><span class="previous"><?php previous_posts_link( 'Previous' ) ?></span> <span class="next"><?php next_posts_link( 'Next' ) ?></span></p><!-- /.navigation -->
+
+	</article><!-- #post-<?php the_ID(); ?> -->
 
 <?php get_footer(); ?>
