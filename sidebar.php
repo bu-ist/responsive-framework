99b1577556cc0a87133b672725f8d925947677ec
Mike Burns <mgburns@bu.edu>
2014-11-01 20:26:08 -0400

    Use sidebar templates.
    Adds sidebar.php (right sidebar) and sidebar-bottom.php (footbar).
    Reduces redundant markup in individual templates.

--- sidebar.php
+++ sidebar.php
@@ -1,6 +1,10 @@
-<?php ?>
-<section role="complementary" class="secondary-content">
-	<?php if ( ! function_exists( 'dynamic_sidebar' )
-	|| ! dynamic_sidebar() ) : ?>
-	<?php endif; ?>
-</section>
+<?php if ( is_active_sidebar( 'right-content-area' ) ): ?>
+	<aside id="right-content-area" class="col-md-4">
+
+		<?php if ( is_page_template( 'page-templates/calendar.php' ) ) {
+			bu_flexi_calendar_sidebar();
+		} ?>
+
+		<?php dynamic_sidebar( 'right-content-area' ); ?>
+	</aside>
+<?php endif; ?>
