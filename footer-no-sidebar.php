29dc1b3b5b8d4628a764e34d63e475d18717211e
Mike Burns <mgburns@bu.edu>
2014-11-03 11:19:21 -0500

    Remove unused footer template.

--- footer-no-sidebar.php
+++
@@ -1,9 +0,0 @@
-</div><!-- .container -->
-</div><!-- #page_wrapper -->
-<footer role="contentinfo">
-	<p>&copy;<?php echo date( 'Y' ); ?> <a href="#top" title="Jump back to top">&#8593;</a></p>
-</footer>
-<?php wp_footer(); ?>
-
-</body>
-</html>
