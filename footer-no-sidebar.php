3957bd968d7c5d4a1abc8b31cccac6c98fecabf2
jonpb <jonpb@bu.edu>
2014-11-05 14:51:21 -0500

    Moved .contentWrapper, .wrapper below footer
    Moved .contentWrapper, .wrapper below footer to support left-hand nav-layout option

--- footer-no-sidebar.php
+++ footer-no-sidebar.php
@@ -1,11 +1,11 @@
 			</div><!-- .container -->
+	
+			<footer role="contentinfo">
+				<p>&copy;<?php echo date( 'Y' ); ?> <a href="#top" title="Jump back to top">&#8593;</a></p>
+			</footer>
+			
+			<?php wp_footer(); ?>
 		</div><!-- .contentWrapper -->
-
-		<footer role="contentinfo">
-			<p>&copy;<?php echo date( 'Y' ); ?> <a href="#top" title="Jump back to top">&#8593;</a></p>
-		</footer>
-		
-		<?php wp_footer(); ?>
 	</div><!-- .wrapper -->
 </body>
 </html>
