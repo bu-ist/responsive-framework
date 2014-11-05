5ac62c779e31c011adb4de81a36d1a1234e101cd
Scott Dasse <sdasse@bu.edu>
2014-11-05 09:44:16 -0500

    Readding .contentWrapper and tweaking indentation
    It is apparently necessary for side nav. And the indentation is correct in header/footer.

--- footer-no-sidebar.php
+++ footer-no-sidebar.php
@@ -1,9 +1,11 @@
-</div><!-- .container -->
-</div><!-- .wrapper -->
+			</div><!-- .container -->
+		</div><!-- .contentWrapper -->
 
-<footer role="contentinfo">
-	<p>&copy;<?php echo date( 'Y' ); ?> <a href="#top" title="Jump back to top">&#8593;</a></p>
-</footer>
-<?php wp_footer(); ?>
+		<footer role="contentinfo">
+			<p>&copy;<?php echo date( 'Y' ); ?> <a href="#top" title="Jump back to top">&#8593;</a></p>
+		</footer>
+		
+		<?php wp_footer(); ?>
+	</div><!-- .wrapper -->
 </body>
 </html>
