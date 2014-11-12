024973c6498595161dce7bb947898bc2b7c889d7
Andy <jarader@bu.edu>
2014-11-12 16:29:53 -0500

    Changing IDs to Classes
    adding classes for brand, siteName, siteDescription

--- template-parts/header-l-sidenav.php
+++ template-parts/header-l-sidenav.php
@@ -3,12 +3,12 @@
  * Header layout with left side navigation bar.
  */
 ?>
-<div id="brand">
-	<a id="siteName" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home" class="logo">
+<div class="brand">
+	<a class="siteName" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
 		Boston University <span><?php bloginfo( 'name' ); ?></span>
 	</a>
 
-	<p class="desc"><?php bloginfo( 'description' ); ?></p>
+	<p class="siteDescription"><?php bloginfo( 'description' ); ?></p>
 
 	<a href="#quicksearch" class="searchToggle"><span>Search</span></a>
 
