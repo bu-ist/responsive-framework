9b037cc102f2cfc6488faa90df503049c31973c0
Mike Burns <mgburns@bu.edu>
2014-11-14 11:51:15 -0500

    Introduces the BU_RESPONSIVE_LAYOUT constant.
    Child themes can define this constant in `functions.php` to force one of the layout options (e.g. default, topNav, sideNav, noNav).

--- inc/settings.php
+++ inc/settings.php
@@ -6,9 +6,15 @@
 /**
  * Returns layout slug for currently active theme layout.
  *
+ * Child themes can force a specific layout option by defining the BU_RESPONSIVE_LAYOUT
+ * constant using one of the layout slugs (e.g. topNav, sideNav, etc.).
+ *
  * @see  responsive_layout_options()
  */
 function responsive_layout() {
+	if ( defined( 'BU_RESPONSIVE_LAYOUT' ) && array_key_exists( BU_RESPONSIVE_LAYOUT, responsive_layout_options() ) ) {
+		return BU_RESPONSIVE_LAYOUT;
+	}
 	return get_option( 'burf_setting_layout', 'default' );
 }
 
