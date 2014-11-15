862479ab65b6d5db64c3d871a835fe82331d79e9
Scott Dasse <sdasse@bu.edu>
2014-11-14 20:57:41 -0500

    Adding modified comments.php from _s

--- comments.php
+++ comments.php
@@ -1,67 +1,43 @@
-<?php ?>
-
-<aside class="comments">
-	<?php if ( post_password_required() ) : ?>
-	<p class="nopassword"><?php _e( 'This post is password protected. Enter the password to view any comments.' ); ?></p>
-</aside>
-
-<?php
-/* Stop the rest of comments.php from being processed,
-		 * but don't kill the script entirely -- we still have
-		 * to fully load the template.
-		 */
-return;
-endif;
+<?php if ( post_password_required() ) {
+	return;
+}
 ?>
 
-<?php
-// You can start editing here -- including this comment!
-?>
+<section id="comments">
 
-<?php if ( have_comments() ) : ?>
-			<h2><?php
-	printf( _n( 'One Response to %2$s', '%1$s Responses to %2$s', get_comments_number() ),
-		number_format_i18n( get_comments_number() ), get_the_title() );
-?></h2>
+	<?php if ( have_comments() ) : ?>
+		<h2 class="comments-title">
+			<?php
+				printf( _nx( 'One comment on %2$s', '%1$s comments on %2$s', get_comments_number(), 'comments title', '_s' ),
+					number_format_i18n( get_comments_number() ), '<strong>' . get_the_title() . '</strong>' );
+			?>
+		</h2>
 
-<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
-	<ul class="navigation">
-		<li class="older">
-			<?php previous_comments_link( __( 'Older Comments' ) ); ?>
-		</li>
-		<li class="newer">
-			<?php next_comments_link( __( 'Newer Comments' ) ); ?>
-		</li>
-	</ul>
-<?php endif; // check for comment navigation ?>
+		<ol class="comment-list">
+			<?php
+				wp_list_comments( array(
+					'style'      => 'ol',
+					'short_ping' => true,
+				) );
+			?>
+		</ol>
 
-<ol class="commentlist">
-	<?php wp_list_comments(); ?>
-</ol>
+		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
+		<nav class="commentNav" role="navigation">
+			<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', '_s' ) ); ?></div>
+			<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', '_s' ) ); ?></div>
+		</nav>
+		<?php endif; // check for comment navigation ?>
 
-<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
-	<ul class="navigation">
-		<li class="older">
-			<?php previous_comments_link( __( 'Older Comments' ) ); ?>
-		</li>
-		<li class="newer">
-			<?php next_comments_link( __( 'Newer Comments' ) ); ?>
-		</li>
-	</ul>
-<?php endif; // check for comment navigation ?>
+	<?php endif; // have_comments() ?>
 
-<?php else : // or, if we don't have comments:
+	<?php
+		// If comments are closed and there are comments, let's leave a little note, shall we?
+		if ( ! comments_open() && '0' != get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
+	?>
+		<p class="no-comments"><?php _e( 'Comments are closed.', '_s' ); ?></p>
+	<?php endif; ?>
 
-	/* If there are no comments and comments are closed,
-	 * let's leave a little note, shall we?
-	 */
-	if ( ! comments_open() && is_single() ) :
-?>
-	<p class="nocomments"><?php _e( 'Comments are closed.' ); ?></p>
-<?php endif; // end ! comments_open() ?>
+	<?php comment_form(); ?>
 
-<?php endif; // end have_comments() ?>
-
-<?php comment_form(); ?>
-
-</aside><!-- .comments -->
+</section>
