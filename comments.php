c704485f6098150dcbae0ca9655626eb7fd07233
Scott Dasse <sdasse@bu.edu>
2014-11-14 23:00:42 -0500

    Adding styles for comments, modifying markup

--- comments.php
+++ comments.php
@@ -3,29 +3,30 @@
 }
 ?>
 
-<section id="comments">
+<section id="comments" class="comments-area">
 
 	<?php if ( have_comments() ) : ?>
 		<h2 class="comments-title">
 			<?php
-				printf( _nx( 'One comment on %2$s', '%1$s comments on %2$s', get_comments_number(), 'comments title', '_s' ),
-					number_format_i18n( get_comments_number() ), '<strong>' . get_the_title() . '</strong>' );
+				printf( _nx( 'One comment', '%1$s comments', get_comments_number(), 'comments title', '_s' ),
+					number_format_i18n( get_comments_number() ) );
 			?>
 		</h2>
 
-		<ol class="comment-list">
+		<ol class="comments-list">
 			<?php
 				wp_list_comments( array(
 					'style'      => 'ol',
 					'short_ping' => true,
+					'max_depth'  => '3',
 				) );
 			?>
 		</ol>
 
 		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
-		<nav class="commentNav" role="navigation">
-			<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', '_s' ) ); ?></div>
-			<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', '_s' ) ); ?></div>
+		<nav class="comments-nav" role="navigation">
+			<div class="comments-commentNav-previous"><?php previous_comments_link( __( '&larr; Older Comments', '_s' ) ); ?></div>
+			<div class="comments-commentNav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', '_s' ) ); ?></div>
 		</nav>
 		<?php endif; // check for comment navigation ?>
 
@@ -35,9 +36,17 @@
 		// If comments are closed and there are comments, let's leave a little note, shall we?
 		if ( ! comments_open() && '0' != get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
 	?>
-		<p class="no-comments"><?php _e( 'Comments are closed.', '_s' ); ?></p>
+		<p class="comments-closed"><?php _e( 'Comments are closed.', '_s' ); ?></p>
 	<?php endif; ?>
 
-	<?php comment_form(); ?>
+	<?php $comments_args = array(
+        // change the title of the reply section
+        'title_reply'=>'Post Your Comment',
+        // remove "Text or HTML to be displayed after the set of comment fields"
+        'comment_notes_after' => '',
+        // redefine your own textarea (the comment body)
+        'comment_field' => '<p class="comment-form-comment"><label for="comment">' . _x( 'Comment', 'noun' ) . '</label><br /><textarea id="comment" name="comment" aria-required="true"></textarea></p>',
+	);
+	comment_form($comments_args); ?>
 
 </section>
