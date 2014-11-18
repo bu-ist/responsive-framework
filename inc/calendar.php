59d737176c425ae6259d74c248be96381a9d9a2d
Mike Burns <mgburns@bu.edu>
2014-11-18 08:10:29 -0500

    Calendar cleanup
    - Restores topic browser sidebar for calendar template
    - Introduces responsive_primary_sidebar, borrowing logic from Flexi.
    - Cleanup in inc/calendar.php

--- inc/calendar.php
+++ inc/calendar.php
@@ -3,26 +3,27 @@
  * BU Calendar plugin support functions & templates.
  */
 
-/*
- * @global object $buCalendar
- * @global array $topics
- * @global int $timestamp
- * @global int $yyyymmdd
+/**
+ * Display the calendar widget markup.
+ *
+ * @global  $buCalendar Calendar plugin instance.
+ *
+ * @param  array  $args [description]
+ * @return [type]       [description]
  */
-
-function bu_flexi_calendar_sidebar( $args = array() ) {
+function responsive_calendar_sidebar( $args = array() ) {
 	global $post, $buCalendar, $topics, $timestamp, $yyyymmdd;
 
 	$defaults = array(
 		'calendar_id'   => array_key_exists( 'cid', $_GET ) ? intval( $_GET['cid'] ) : get_option( 'bu_calendar_id' ),
 		'show_topics'   => true,
 		'calendar_uri'  => get_permalink( $post ),
-		'page_template' => 'calendar.php',
+		'page_template' => 'page-templates/calendar.php',
 	);
 
 	$args = wp_parse_args( $args, $defaults );
 
-	$args = apply_filters( 'bu_flexi_calendar_sidebar', $args );
+	$args = apply_filters( 'responsive_calendar_sidebar', $args );
 
 	$all_topics_url = add_query_arg( 'date', date( 'Ymd', $timestamp ), $args['calendar_uri'] );
 
@@ -44,64 +45,20 @@ function bu_flexi_calendar_sidebar( $args = array() ) {
 		</div>
 		<?php endif; ?>
 		<?php
-
-		do_action( 'bu_flexi_calendar_sidebar_content' );
 	}
 }
 
-function bu_flexi_micro_calendar( $args = array() ) {
-	global $post, $buCalendar, $topics, $timestamp, $yyyymmdd;
-
-	$defaults = array(
-		'calendar_id'   => array_key_exists( 'cid', $_GET ) ? intval( $_GET['cid'] ) : get_option( 'bu_calendar_id' ),
-		'show_topics'   => true,
-		'calendar_uri'  => get_permalink( $post ),
-		'page_template' => 'calendar.php',
-	);
-
-	$args = wp_parse_args( $args, $defaults );
-
-	$args = apply_filters( 'bu_flexi_calendar_micro', $args );
-
-	$all_topics_url = add_query_arg( 'date', date( 'Ymd', $timestamp ), $args['calendar_uri'] );
-
-	if ( ! empty( $_GET['cid'] ) ) {
-		$all_topics_url = add_query_arg( 'cid', intval( $_GET['cid'] ), $all_topics_url );
-	}
-	?>
-	<div id="micro_calendar" class="container sub">
-		<div class="widget">
-		<h2 class="widgettitle">Event Dates &amp; Topics</h2>
-			<div class="calendar-nav">
-				<div class="month">
-					<?php echo $buCalendar->buildMonthCalendar( $yyyymmdd ); ?>
-				</div>
-				<?php if ( $args['show_topics'] ): ?>
-				<div id="calendar-topics" class="topics">
-					<h4>Event Topics</h4>
-					<p><a href="<?php echo $all_topics_url; ?>">All Topics</a></p>
-				<?php echo $buCalendar->buildTopicTree( $topics ); ?>
-				</div>
-			<?php endif; ?>
-			</div>
-			<?php do_action( 'bu_flexi_calendar_micro_content' ); ?>
-		</div>
-	</div>
-	<?php
-}
-
-/* - - - - - - - - - - - - - - - - -
-  BU Calendar Widget Formats
-  - - - - - - - - - - - - - - - - - */
-
-function bu_flexi_calendar_widget_formats( $formats ) {
+/**
+ * Register custom calendar widget formats
+ */
+function responsive_calendar_widget_formats( $formats ) {
 
 	unset( $formats['plain'] );
 	unset( $formats['big'] );
 
 	$formats['full-date'] = array(
 		'label'    => 'Full Date',
-		'callback' => 'bu_flexi_calendar_full_date',
+		'callback' => 'responsive_calendar_format_callback',
 	);
 
 	$formats['graphic'] = array(
@@ -112,7 +69,15 @@ function bu_flexi_calendar_widget_formats( $formats ) {
 	return $formats;
 }
 
-function bu_flexi_calendar_full_date( $events, $base_url, $calendar_id = null ) {
+/**
+ * Calendar widget format display callbacks
+ *
+ * @param  array  $events      Event list
+ * @param  string $base_url    Calendar URL
+ * @param  int    $calendar_id Calendar ID
+ * @return string              Calendar widget markup
+ */
+function responsive_calendar_format_callback( $events, $base_url, $calendar_id = null ) {
 	$output = '';
 
 	if ( ( is_array( $events ) ) && ( count( $events ) > 0 ) ) {
@@ -137,4 +102,4 @@ function bu_flexi_calendar_full_date( $events, $base_url, $calendar_id = null ) 
 	return $output;
 }
 
-add_filter( 'bu_calendar_widget_formats', 'bu_flexi_calendar_widget_formats', 12, 1 );
+add_filter( 'bu_calendar_widget_formats', 'responsive_calendar_widget_formats', 12, 1 );
