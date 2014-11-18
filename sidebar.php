5f0a4b8a34efacb93c2f0b40b1dff8e814e87f70
Mike Burns <mgburns@bu.edu>
2014-11-18 08:10:29 -0500

    Calendar cleanup
    - Restores topic browser sidebar for calendar template
    - Introduces responsive_primary_sidebar, borrowing logic from Flexi.
    - Cleanup in inc/calendar.php

--- sidebar.php
+++ sidebar.php
@@ -1,10 +1,3 @@
-<?php if ( is_active_sidebar( 'sidebar' ) ): ?>
 	<aside class="sidebar">
-
-		<?php if ( is_page_template( 'page-templates/calendar.php' ) ) {
-			bu_flexi_calendar_sidebar();
-		} ?>
-
-		<?php dynamic_sidebar( 'sidebar' ); ?>
+		<?php responsive_primary_sidebar(); ?>
 	</aside>
-<?php endif; ?>
