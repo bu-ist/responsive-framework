1d52e9911bb70a1d92d525940f97ae75eab39fce
Mike Burns <mgburns@bu.edu>
2014-11-19 14:53:08 -0500

    Switch to custom menus for social media links
    Also removes unused 'Primary Menu' location.

--- footer.php
+++ footer.php
@@ -10,19 +10,15 @@
 			}
 
 			$footer_contact = get_option( 'burf_setting_footer_contact' );
-			$footer_social_fb = get_option( 'burf_setting_footer_social_fb' );
-			$footer_social_tw = get_option( 'burf_setting_footer_social_tw' );
-			$footer_social_ig = get_option( 'burf_setting_footer_social_ig' );
-			$footer_social_yt = get_option( 'burf_setting_footer_social_yt' );
-			$footer_social_li = get_option( 'burf_setting_footer_social_li' );
-
 			if ( $footer_contact ) { echo $footer_contact; }
 
-			if ( $footer_social_fb ) { echo '<a class="social-fb" href="' . $footer_social_fb . '">facebook</a>'; }
-			if ( $footer_social_tw ) { echo '<a class="social-tw" href="' . $footer_social_fb . '">twitter</a>'; }
-			if ( $footer_social_ig ) { echo '<a class="social-ig" href="' . $footer_social_fb . '">instagram</a>'; }
-			if ( $footer_social_yt ) { echo '<a class="social-yt" href="' . $footer_social_fb . '">youtube</a>'; }
-			if ( $footer_social_li ) { echo '<a class="social-li" href="' . $footer_social_fb . '">linkedin</a>'; }
+			// Social media links
+			wp_nav_menu( array(
+				'theme_location' => 'social',
+				'items_wrap'     => '<ul>%3$s</ul>',
+				'container'      => false,
+				'fallback_cb'    => false,
+			) );
 
 			?>
 			</footer>
