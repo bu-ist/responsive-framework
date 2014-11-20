c1da506c17a67769c6d053ede7cf12715293ebbe
Andy <jarader@bu.edu>
2014-11-20 17:03:05 -0500

    Make Profiles listings more semantic

--- bu-profiles-mini.php
+++ bu-profiles-mini.php
@@ -9,18 +9,14 @@
 			<?php while ( $query->have_posts() ): ?>
 				<?php $query->the_post(); ?>
 				<li>
-					<div class="thumb_container">
-						<?php if ( function_exists( 'bu_thumbnail' ) ): $thumb_args = array( 'maxwidth' => 150, 'maxheight' => 150 ); ?>
-							<?php bu_thumbnail( '', '', $thumb_args ); ?>
-						<?php endif; ?>
-					</div>
-					<div class="content_container">
-						<?php bu_profile_detail( 'first_name', array( 'before' => '<span class="profile-name">' ) ); ?>
-						<?php bu_profile_detail( 'last_name', array( 'after' => '</span>' ) ); ?>
-						<?php bu_profile_detail( 'email', array( 'format' => 'email', 'before' => '<span class="email_container">', 'after' => '</span>' ) ); ?>
-						<div class="content_summary">
-							<?php bu_profile_detail( '_bu_page_description', array( 'before' => '<p>', 'after' => '</p>', 'format' => 'multi-line' ) ); ?>
-						</div>
+					<?php if ( function_exists( 'bu_thumbnail' ) ): $thumb_args = array( 'maxwidth' => 150, 'maxheight' => 150 ); ?>
+						<?php bu_thumbnail( '<figure>', '</figure>', $thumb_args ); ?>
+					<?php endif; ?>
+					<div class="details">
+						<?php bu_profile_detail( 'first_name', array( 'before' => '<p class="name">' ) ); ?>
+						<?php bu_profile_detail( 'last_name', array( 'after' => '</p>' ) ); ?>
+						<?php bu_profile_detail( 'email', array( 'format' => 'email', 'before' => '<p class="email">', 'after' => '</p>' ) ); ?>
+						<?php bu_profile_detail( '_bu_page_description', array( 'before' => '<p class="summary">', 'after' => '</p>', 'format' => 'multi-line' ) ); ?>
 					</div>
 				</li>
 			<?php endwhile; ?>
