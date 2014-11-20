8dcbd5701db4c756235c3d61027c4ab5d9cac28c
Andy <jarader@bu.edu>
2014-11-20 17:03:05 -0500

    Make Profiles listings more semantic

--- bu-profiles-basic.php
+++ bu-profiles-basic.php
@@ -11,8 +11,8 @@
 				<?php $query->the_post(); ?>
 				<li<?php if ( bu_profile_detail( 'title', array( 'echo' => false ) ) ): ?> class="has-title"<?php endif; ?>>
 					<a href="<?php the_permalink(); ?>">
-						<?php if ( function_exists( 'bu_thumbnail' ) ): $thumb_args = array( 'maxwidth' => 36, 'maxheight' => 36 ); ?>
-							<?php bu_thumbnail( '', '', $thumb_args ); ?>
+						<?php if ( function_exists( 'bu_thumbnail' ) ): $thumb_args = array( 'maxwidth' => 60, 'maxheight' => 60 ); ?>
+							<?php bu_thumbnail( '<figure>', '</figure>', $thumb_args ); ?>
 						<?php endif; ?>
 						<p class="profile-name"><?php bu_profile_detail( 'first_name' ); ?> <?php bu_profile_detail( 'last_name' ); ?></p>
 						<?php bu_profile_detail( 'title', array( 'before' => '<p class="profile-title">', 'after' => '</p>', 'format' => 'multi-line' ) ); ?>
