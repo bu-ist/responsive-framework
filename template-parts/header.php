04d0714501f0d7e9305049128b0b72c64d98c02e
Scott Dasse <sdasse@bu.edu>
2014-11-25 12:28:15 -0500

    Renamed containers to comply with standards

--- template-parts/header.php
+++ template-parts/header.php
@@ -11,8 +11,8 @@
 	<nav class="utilityNav" role="navigation"><?php responsive_utility_nav(); ?></nav>
 </div>
 
-<nav class="navContainer" role="navigation">
-	<a href="#primaryNav" class="navToggle"><span>Menu</span></a>
+<nav class="primaryNav" role="navigation">
+	<a href="#primaryNav-menu" class="navToggle"><span>Menu</span></a>
 	<a href="#quicksearch" class="searchToggle"><span>Search</span></a>
 
 	<?php responsive_primary_nav(); ?>
