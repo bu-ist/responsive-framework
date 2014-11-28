f7d547fdc4cd53f1753ba960bd4abe5a432032df
Scott Dasse <sdasse@bu.edu>
2014-11-28 08:06:50 -0500

    Adds second stateful class to body when search is open

--- js/script.js
+++ js/script.js
@@ -7,13 +7,15 @@ jQuery(function($) {
 		$("body").toggleClass("nav-open");
 		
 		$(".searchToggle, #quicksearch").removeClass("is-open");
+		$("body").removeClass("search-open");
 		
 	});
 	
 	$(".searchToggle").on("click", function(e){
 		e.preventDefault();
-		$(this).toggleClass("is-open");
+		$(".searchToggle").toggleClass("is-open");
 		$("#quicksearch").toggleClass("is-open");
+		$("body").toggleClass("search-open");
 		
 		$("nav, .navToggle").removeClass("is-open");
 		$("body").removeClass("nav-open");
