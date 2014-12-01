7ffb82d4bbf7be5e2d3d8aba530eef3d714a520c
Mike Burns <mgburns@bu.edu>
2014-12-01 06:46:25 -0500

    Improvements to responsive_utility_nav
    - Don't show fallback (wp_list_pages) when utility menu is not set
    - Bring markup into template tag to reduce code duplication
    - Update menu attributes for consistency with primary nav

--- template-parts/header.php
+++ template-parts/header.php
@@ -8,7 +8,7 @@
 
 	<p class="siteDescription"><?php bloginfo( 'description' ); ?></p>
 
-	<nav class="utilityNav" role="navigation"><?php responsive_utility_nav(); ?></nav>
+	<?php responsive_utility_nav(); ?>
 </div>
 
 <nav class="primaryNav" role="navigation">
@@ -16,8 +16,7 @@
 	<a href="#quicksearch" class="searchToggle"><span>Search</span></a>
 
 	<?php responsive_primary_nav(); ?>
-
-	<nav class="utilityNav" role="navigation"><?php responsive_utility_nav(); ?></nav>
+	<?php responsive_utility_nav(); ?>
 </nav>
 
 <?php responsive_search_form(); ?>
