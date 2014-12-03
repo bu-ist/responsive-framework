39f94414476772a0a2d80089cb6e0aa0f8c62e77
Mike Burns <mgburns@bu.edu>
2014-12-03 08:06:28 -0500

    Upgrade routine updates
    Adds migration for sidebars and content banner positions.
    Cleans up page template migration.

--- inc/upgrade.php
+++ inc/upgrade.php
@@ -42,22 +42,70 @@ add_action( 'init', 'responsive_framework_upgrade' );
  * Upgrade for 0.9.1.
  *
  * - Page template renaming
+ * - Banner position renaming
+ * - Sidebar renaming
  */
 function responsive_upgrade_091() {
 	global $wpdb;
 
+	// Rename page templates
 	error_log( __FUNCTION__ . ' - Migrating page templates...' );
-	$relocate_map = array(
+	$template_map = array(
 		'calendar.php'        => 'page-templates/calendar.php',
 		'news.php'            => 'page-templates/news.php',
 		'page-nosidebars.php' => 'page-templates/no-sidebars.php',
+		// 'page-homepage.php'   => 'page-templates/homepage.php',  // TODO: Determine whether Research / Pardee will use this or front-page.php
 		'profiles.php'        => 'page-templates/profiles.php',
 		);
-	$pt_query = sprintf( 'SELECT post_id, meta_value FROM %s WHERE meta_key = "%s" AND meta_value IN ("%s")',
-		$wpdb->postmeta, '_wp_page_template', implode( '","', array_keys( $relocate_map ) ) );
-	$results = $wpdb->get_results( $pt_query );
+	$template_query = sprintf( 'SELECT post_id, meta_value FROM %s WHERE meta_key = "_wp_page_template" AND meta_value IN ("%s")',
+		$wpdb->postmeta, implode( '","', array_keys( $template_map ) )
+		);
+	$results = $wpdb->get_results( $template_query );
 
 	foreach ( $results as $result ) {
-		update_post_meta( $result->post_id, '_wp_page_template', $relocate_map[ $result->meta_value ] );
+		update_post_meta( $result->post_id, '_wp_page_template', $template_map[ $result->meta_value ] );
 	}
+
+	// Rename banner positions
+	error_log( __FUNCTION__ . ' - Migrating content banners...' );
+	$banner_map = array(
+		'content-width' => 'contentWidth',
+		'page-width'    => 'pageWidth',
+		'window-width'  => 'windowWidth',
+		);
+	$banner_query = sprintf( 'SELECT post_id, meta_value FROM %s WHERE meta_key = "_bu_banner"',
+		$wpdb->postmeta
+		);
+	$results = $wpdb->get_results( $banner_query );
+
+	foreach ( $results as $result ) {
+		$banner = maybe_unserialize( $result->meta_value );
+		if ( is_array( $banner ) ) {
+			if ( array_key_exists( 'position', $banner ) && in_array( $banner['position'], array_keys( $banner_map ) ) ) {
+				error_log( __FUNCTION__ . ' - Migrating banner position from ' . $banner['position'] . ' to ' . $banner_map[ $banner['position'] ] );
+				$banner['position'] = $banner_map[ $banner['position'] ];
+				update_post_meta( $result->post_id, '_bu_banner', $banner );
+			} else if ( ! array_key_exists( 'position', $banner ) || empty( $banner['position'] ) ) {
+				error_log( __FUNCTION__ . ' - Resetting empty banner position to default (contentWidth)' );
+				// Reset to default
+				$banner['position'] = 'contentWidth';
+				update_post_meta( $result->post_id, '_bu_banner', $banner );
+			}
+		}
+	}
+
+	// Rename sidebars
+	error_log( __FUNCTION__ . ' - Migrating sidebars...' );
+	$sidebars_map = array(
+		'right-content-area'  => 'sidebar',
+		'bottom-content-area' => 'footbar',
+		);
+	$sidebars_widgets = wp_get_sidebars_widgets();
+	foreach ( $sidebars_map as $from => $to ) {
+		if ( array_key_exists( $from, $sidebars_widgets ) ) {
+			$sidebars_widgets[ $to ] = $sidebars_widgets[ $from ];
+			unset( $sidebars_widgets[ $from ] );
+		}
+	}
+	wp_set_sidebars_widgets( $sidebars_widgets );
 }
