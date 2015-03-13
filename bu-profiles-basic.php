9e39ed1a7fcfd9169ed630be1c6cea53bf0be254
Mike Burns <mgburns@bu.edu>
2015-03-13 16:45:04 +0200

    Move profile format contents into template parts

--- bu-profiles-basic.php
+++ bu-profiles-basic.php
@@ -7,17 +7,8 @@
 <?php if ( $query->have_posts() ): ?>
 	<div class="profile-listing">
 		<ul class="basic">
-			<?php while ( $query->have_posts() ): ?>
-				<?php $query->the_post(); ?>
-				<li<?php if ( bu_profile_detail( 'title', array( 'echo' => false ) ) ): ?> class="has-title"<?php endif; ?>>
-					<a href="<?php the_permalink(); ?>">
-						<?php if ( function_exists( 'bu_thumbnail' ) ): $thumb_args = array( 'maxwidth' => 60, 'maxheight' => 60 ); ?>
-							<?php bu_thumbnail( '<figure>', '</figure>', $thumb_args ); ?>
-						<?php endif; ?>
-						<p class="profile-name"><?php bu_profile_detail( 'first_name' ); ?> <?php bu_profile_detail( 'last_name' ); ?></p>
-						<?php bu_profile_detail( 'title', array( 'before' => '<p class="profile-title">', 'after' => '</p>', 'format' => 'multi-line' ) ); ?>
-					</a>
-				</li>
+			<?php while ( $query->have_posts() ) : $query->the_post(); ?>
+				<?php get_template_part( 'template-parts/content', 'profiles' ); ?>
 			<?php endwhile; ?>
 		</ul>
 	</div><!--/.profile-listing-->
