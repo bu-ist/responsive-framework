f836f5e4176b4910df85b9d39008c13b1d398c33
Mike Burns <mgburns@bu.edu>
2015-04-07 13:33:02 +0300

    Add support for BU Sharing plugin
    - Add `responsive_share_tools()` template tag
    - Disable default share display location, counts
    - Ensure BU icon font isn't loaded twice

---
+++ inc/sharing.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * BU Sharing integration
+ */
+
+/**
+ * Modify default share tools configuration.
+ */
+function responsive_sharing_setup() {
+	// Disable default display location
+	remove_filter( 'the_content', 'sharing_display', 19 );
+	remove_filter( 'the_excerpt', 'sharing_display', 19 );
+
+	// Disable share counts by default
+	add_filter( 'bu_sharing_counts', '__return_false' );
+}
+
+add_action( 'init', 'responsive_sharing_setup' );
+
+/**
+ * Prevent duplicate loading of icon fonts by BU Sharing.
+ */
+function responsive_dequeue_sharing_fonts() {
+	if ( wp_style_is( 'bu-icon-font' ) ) {
+		wp_dequeue_style( 'bu-icon-font' );
+	}
+}
+
+add_action( 'wp_head', 'responsive_dequeue_sharing_fonts', 2 );
+
+/**
+ * Display share tools configured via BU Sharing plugin.
+ *
+ * If the BU Sharing plugin is not activated this function will do nothing.
+ *
+ * @param  string $text  Content to display before share tools. Will precede user-provided sharing label.
+ * @param  boolean       Whether or not to immediately echo or return the output. Default true.
+ */
+function responsive_share_tools( $text = '', $echo = true ) {
+	if ( function_exists( 'sharing_display' ) ) {
+		return sharing_display( $text, $echo );
+	}
+}
