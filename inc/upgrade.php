ef34714c4e47bb8bab8edbf5ed4e74b0877c0a07
Mike Burns <mgburns@bu.edu>
2015-04-10 12:00:35 +0300

    Add hooks to uprade routines
    These hooks allow Research and Pardee school to define custom templates, banner positions, etc. when they are migrated.

--- inc/upgrade.php
+++ inc/upgrade.php
@@ -26,6 +26,8 @@ function responsive_framework_upgrade() {
 				responsive_upgrade_091();
 			}
 
+			do_action( 'responsive_framework_upgrade', $db_version, RESPONSIVE_FRAMEWORK_VERSION );
+
 			error_log( __FUNCTION__ . ' - Updating framework version in DB: ' . $db_version . ' -> ' . RESPONSIVE_FRAMEWORK_VERSION );
 			update_option( '_responsive_framework_version', RESPONSIVE_FRAMEWORK_VERSION );
 		}
@@ -50,17 +52,18 @@ function responsive_upgrade_091() {
 
 	// Rename page templates
 	error_log( __FUNCTION__ . ' - Migrating page templates...' );
-	$template_map = array(
+	$template_map = apply_filters( __FUNCTION__ . '_template_map', array(
 		'calendar.php'        => 'page-templates/calendar.php',
 		'news.php'            => 'page-templates/news.php',
 		'page-nosidebars.php' => 'page-templates/no-sidebars.php',
-		// 'page-homepage.php'   => 'page-templates/homepage.php',  // TODO: Determine whether Research / Pardee will use this or front-page.php
 		'profiles.php'        => 'page-templates/profiles.php',
-		);
+		) );
+
 	$template_query = sprintf( 'SELECT post_id, meta_value FROM %s WHERE meta_key = "_wp_page_template" AND meta_value IN ("%s")',
 		$wpdb->postmeta, implode( '","', array_keys( $template_map ) )
 		);
 	$results = $wpdb->get_results( $template_query );
+	error_log( __FUNCTION__ . ' - Posts to migrate: ' . count( $results ) );
 
 	foreach ( $results as $result ) {
 		update_post_meta( $result->post_id, '_wp_page_template', $template_map[ $result->meta_value ] );
@@ -68,11 +71,11 @@ function responsive_upgrade_091() {
 
 	// Rename banner positions
 	error_log( __FUNCTION__ . ' - Migrating content banners...' );
-	$banner_map = array(
+	$banner_map = apply_filters( __FUNCTION__ . '_banner_map', array(
 		'content-width' => 'contentWidth',
 		'page-width'    => 'pageWidth',
 		'window-width'  => 'windowWidth',
-		);
+		) );
 	$banner_query = sprintf( 'SELECT post_id, meta_value FROM %s WHERE meta_key = "_bu_banner"',
 		$wpdb->postmeta
 		);
@@ -82,7 +85,6 @@ function responsive_upgrade_091() {
 		$banner = maybe_unserialize( $result->meta_value );
 		if ( is_array( $banner ) ) {
 			if ( array_key_exists( 'position', $banner ) && in_array( $banner['position'], array_keys( $banner_map ) ) ) {
-				error_log( __FUNCTION__ . ' - Migrating banner position from ' . $banner['position'] . ' to ' . $banner_map[ $banner['position'] ] );
 				$banner['position'] = $banner_map[ $banner['position'] ];
 				update_post_meta( $result->post_id, '_bu_banner', $banner );
 			} else if ( ! array_key_exists( 'position', $banner ) || empty( $banner['position'] ) ) {
@@ -96,10 +98,10 @@ function responsive_upgrade_091() {
 
 	// Rename sidebars
 	error_log( __FUNCTION__ . ' - Migrating sidebars...' );
-	$sidebars_map = array(
+	$sidebars_map = apply_filters( __FUNCTION__ . '_sidebars_map', array(
 		'right-content-area'  => 'sidebar',
 		'bottom-content-area' => 'footbar',
-		);
+		) );
 	$sidebars_widgets = wp_get_sidebars_widgets();
 	foreach ( $sidebars_map as $from => $to ) {
 		if ( array_key_exists( $from, $sidebars_widgets ) ) {
