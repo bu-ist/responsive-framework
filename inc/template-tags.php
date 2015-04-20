fbb87514becd8e47b1ee04cc252314df3b43d379
Mike Burns <mgburns@bu.edu>
2015-04-20 13:46:21 +0300

    Don't display search icon when search is disabled
    Fixes #30.

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -74,6 +74,16 @@ function responsive_comments() {
 }
 
 /**
+ * Whether or not search form should be displayed.
+ */
+function responsive_search_is_enabled() {
+	if ( class_exists( 'BU_SearchForm' ) ) {
+		return BU_SearchForm::isEnabled();
+	}
+	return true;
+}
+
+/**
  * Displays search form for the site based on whether or not there is a site-wide ACL in place
  */
 function responsive_search_form() {
@@ -81,7 +91,7 @@ function responsive_search_form() {
 
 	// Check that search form is enabled
 	if ( function_exists( 'bu_search_form' ) ) {
-		if ( true === BU_SearchForm::isEnabled() ) {
+		if ( responsive_search_is_enabled() ) {
 			$bu_search = true;
 		} else {
 			return;
