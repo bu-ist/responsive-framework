46e6400a4ac91b699cc74b1feccb0471bb901ee1
Eveleen Sung <eksung@bu.edu>
2015-07-16 13:46:49 -0400

    Change h2s to page title in header

--- no-access-bumc.php
+++ no-access-bumc.php
@@ -8,11 +8,10 @@ get_header();
 <section class="no-access not-found">
 
 	<header class="page-header">
-		<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.' ); ?></h1>
+		<h1 class="page-title"><?php _e( 'Access Restricted To BUMC/BMC' ); ?></h1>
 	</header><!-- .page-header -->
 
 	<div class="page-content">
-		<h2>Access Restricted To BUMC/BMC</h2>
 		<p>You are trying to access a page that is restricted to members of the Boston University Medical Campus (BUMC) and Boston Medical Center (BMC). If you are indeed part of the BUMC/BMC community, contact the BUMC Help Desk (<a href="mailto:bumchelp@bu.edu">bumchelp@bu.edu</a> | 617-638-5914) for further assistance, Monday through Friday, 8:30 a.m. to 5 p.m. or visit:</p>
 		<p><a href="http://www.bumc.bu.edu/bumc/oit/network/VPN">http://www.bumc.bu.edu/bumc/oit/network/VPN</a></p>
 	</div><!-- .page-content -->
--- no-access.php
+++ no-access.php
@@ -8,11 +8,10 @@ get_header();
 <section class="no-access not-found">
 
 	<header class="page-header">
-		<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.' ); ?></h1>
+		<h1 class="page-title"><?php _e( 'Access Restricted To BU/BUMC/BMC' ); ?></h1>
 	</header><!-- .page-header -->
 
 	<div class="page-content">
-		<h2>Access Restricted To BU/BUMC/BMC</h2>
 		<p>You are trying to access a page that is restricted to members of the Boston University community.  For further assistance, please see:</p>
 		<p><a href="http://www.bu.edu/pcsc/vpn/">http://www.bu.edu/pcsc/vpn/</a></p>
 		<p><a href="http://www.bumc.bu.edu/bumc/oit/network/VPN">http://www.bumc.bu.edu/bumc/oit/network/VPN</a></p>
