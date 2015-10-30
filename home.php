e8d463772ee3db447ba76a10ca8d3875357088a7
Matt <malakhi@gmail.com>
2015-10-30 11:13:34 -0400

    edits for homepage as a blog, tagline

--- home.php
+++ home.php
@@ -7,8 +7,6 @@ get_header(); ?>
 
 	<?php if ( have_posts() ) : ?>
 
-		<h1>Latest Posts</h1>
-
 	<?php while ( have_posts() ) : the_post(); ?>
 
 		<?php get_template_part( 'template-parts/content' ); ?>
