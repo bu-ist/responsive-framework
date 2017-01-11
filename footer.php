e1d810dda28509f4995bdb7695df49dc12a4e4aa
Jon Desrosiers <desrosj@gmail.com>
2017-01-11 16:33:36 -0500

    Add action hooks before and after the opening and closing `.content` and `.content-container` `<div>`s.

--- footer.php
+++ footer.php
@@ -6,11 +6,45 @@
  */
 
 ?>
+
+				<?php
+					/**
+					 * Fires immediately before the closing content container div.
+					 *
+					 * @since 2.0.0
+					 */
+					do_action( 'r_before_closing_content_container' );
+				?>
 			</div><!-- .content-container -->
+			<?php
+				/**
+				 * Fires immediately after the closing content container div.
+				 *
+				 * @since 2.0.0
+				 */
+				do_action( 'r_after_closing_content_container' );
+			?>
 
 		<?php get_sidebar( 'bottom' ); ?>
 
+			<?php
+				/**
+				 * Fires immediately before the closing content div.
+				 *
+				 * @since 2.0.0
+				 */
+				do_action( 'r_before_closing_content' );
+			?>
 		</div><!-- .content -->
+		<?php
+			/**
+			 * Fires immediately after the closing content div.
+			 *
+			 * @since 2.0.0
+			 */
+			do_action( 'r_after_closing_content' );
+		?>
+
 	</div><!-- .wrapper -->
 
 	<footer class="siteFooter <?php responsive_extra_footer_classes(); ?>" role="contentinfo">
