19002f7ad41d01ed0a012e480219753f8976ff64
Jon Desrosiers <desrosj@gmail.com>
2017-01-11 10:56:46 -0500

    Fix attribute escape call, can’t translate a variable.

--- inc/branding.php
+++ inc/branding.php
@@ -19,7 +19,7 @@ function responsive_branding() {
 
 	$name = get_bloginfo( 'name' );
 ?>
-	<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php esc_attr_e( $name ); ?>" rel="home">
+	<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( $name ); ?>" rel="home">
 		<span class="siteName"><?php echo esc_html( $name ); ?></span>
 	</a>
 <?php
