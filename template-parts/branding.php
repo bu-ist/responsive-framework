955065d28b94ab80d2889d2089893e1a1cbba6ef
Jon Desrosiers <desrosj@gmail.com>
2017-01-13 14:29:55 -0500

    Allow a child theme to override the default branding markup. This will need to be combined with a constant that will short circuit the BU Branding plugin.

---
+++ template-parts/branding.php
@@ -0,0 +1,3 @@
+<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>" rel="home">
+	<span class="siteName"><?php echo esc_html( get_bloginfo( 'name' ) ); ?></span>
+</a>
