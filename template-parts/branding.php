5e11d5476a2c47d7451b882a05a95f929b5a7a75
Jon Desrosiers <desrosj@gmail.com>
2017-01-13 15:23:15 -0500

    Add missing file docblock.

--- template-parts/branding.php
+++ template-parts/branding.php
@@ -1,3 +1,13 @@
+<?php
+/**
+ * Default branding markup.
+ *
+ * This is only used when the BU Branding plugin is inactive, or RESPONSIVE_BRANDING_OVERRIDE is defined as true in a child theme.
+ *
+ * @package Responsive_Framework
+ */
+
+?>
 <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>" rel="home">
 	<span class="siteName"><?php echo esc_html( get_bloginfo( 'name' ) ); ?></span>
 </a>
