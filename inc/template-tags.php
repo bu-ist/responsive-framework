cca25d0b84731bae476d75448a956d7bce4b0cce
Jon Desrosiers <desrosj@gmail.com>
2017-02-02 15:31:34 -0500

    Remove `responsive_quereied_post_types()`. Deprecate `responsive_is_archive_type()` in favor of `is_post_type_archive()` calls. Update `responsive_posts_navigation()` to remove `responsive_is_archive_type()` call. The labels in there need to be further refined, but we can tackle that when we refactor navigation in #156.

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -483,15 +483,22 @@ if ( ! function_exists( 'responsive_posts_navigation' ) ) :
 
 		// Don't print empty markup if there's only one page.
 		if ( $wp_query->max_num_pages >= 2 ) :
-			/**
-			 * @todo Deprecate responsive_archive_type().
-			 */
-			$archive_type = responsive_archive_type( $wp_query );
+			$queried_object = get_queried_object();
+			if ( is_post_type_archive() ) {
+				$archive_type = $queried_object->labels->singular_name;
+			} elseif ( is_tax() || is_category() || is_tag() ) {
+				$taxonomy_object = get_taxonomy( $queried_object->taxonomy );
+
+				$post_type = get_post_type( $taxonomy_object->object_type[0] );
+
+				$archive_type = $post_type->labels->singular_name;
+			}
+
 			$defaults = array(
 				'prev_text'          => '<span class="meta-nav">&larr;</span> Previous',
 				'next_text'          => 'Next <span class="meta-nav">&rarr;</span>',
 				'screen_reader_text' => ucfirst( $archive_type ) . ' navigation',
-				);
+			);
 
 				// Post archive labels are more specific.
 			if ( 'posts' === $archive_type ) {
@@ -812,72 +819,19 @@ function responsive_extra_footer_classes() {
 }
 
 /**
- * Get list of all post types included for the given query
- *
- * @param  WP_Query $query Query object to check. Optional. Defaults to current global query.
- * @return array  Array of post type names
- */
-function responsive_queried_post_types( WP_Query $query = null ) {
-	if ( is_null( $query ) ) {
-		$query = $GLOBALS['wp_query'];
-	}
-
-	$queried_object = $query->get_queried_object();
-
-	// Post = post object.
-	if ( $query->is_single() || $query->is_page() ) {
-		$post_types = array( $queried_object->post_type );
-	} // Post type archive = post type object.
-	elseif ( $query->is_post_type_archive() ) {
-		$post_types = array( $queried_object->name );
-	} // Taxonomy archive = taxonomy object.
-	elseif ( $query->is_tax() || $query->is_category() || $query->is_tag() ) {
-		$tax = get_taxonomy( $queried_object->taxonomy );
-		$post_types = $tax->object_type;
-	} // All other requests default to posts (author archives, date archives, etc.).
-	else {
-		$post_types = array( 'post' );
-	}
-
-	return apply_filters( 'responsive_queried_post_types', $post_types, $query );
-}
-
-/**
- * Determine primary post type of archive query
- *
- * @todo Deprecate and return archive type based on get_quereied_object().
- *
- * @param  WP_Query $query Query object to check. Optional. Defaults to current global query.
- * @return string Post type name. Uses lowercase version of plural (name) label.
- */
-function responsive_archive_type( WP_Query $query = null ) {
-	$post_types = responsive_queried_post_types( $query );
-
-	// Default type.
-	$archive_type = 'posts';
-
-	// Use plural label.
-	if ( is_array( $post_types ) && 1 === count( $post_types ) ) {
-		$pto = get_post_type_object( reset( $post_types ) );
-		if ( $pto ) {
-			$archive_type = strtolower( $pto->label );
-		}
-	}
-
-	return sanitize_key( apply_filters( 'responsive_archive_type', $archive_type, $post_types ) );
-}
-
-/**
  * Is the archive query for the given post type?
  *
- * @todo deprecate in favor of is_category() is_tax, is_post_type_archive() etc.
+ * @deprecated 2.0.0 Use is_post_type_archive()
  *
  * @param  string   $type  Plural post type name for comparison.
  * @param  WP_Query $query Query object to check. Optional. Defaults to current global query.
+ *
  * @return bool
  */
 function responsive_is_archive_type( $type, WP_Query $query = null ) {
-	return ( strtolower( $type ) === responsive_archive_type( $query ) );
+	_deprecated_function( __FUNCTION__, '2.0.0', 'is_post_type_archive()' );
+
+	return is_post_type_archive( $type );
 }
 
 /**
