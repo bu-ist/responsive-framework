cbdcc7745ad5279832399388ba5d531c4cf32e27
Jon Desrosiers <desrosj@gmail.com>
2017-02-06 14:36:08 -0700

    Got testing working locally. Committing to see if Travis works as well.

--- tests/test-sample.php
+++ tests/test-sample.php
@@ -8,13 +8,31 @@
 /**
  * Sample test case.
  */
-class SampleTest extends WP_UnitTestCase {
+class Responsive_Framework_Core_Support_Tests extends WP_UnitTestCase {
+
+	function setUp() {
+		parent::setUp();
+	}
 
 	/**
 	 * A single example test.
 	 */
-	function test_sample() {
-		// Replace this with some actual testing code.
-		$this->assertTrue( true );
+	function test_current_theme() {
+		$current_theme = wp_get_theme();
+		$this->assertEquals( 'Responsive Framework', $current_theme->get( 'Name' ) );
+	}
+
+	function test_theme_support() {
+//		$this->assertTrue( current_theme_supports( 'html5' ) );
+		$this->assertTrue( current_theme_supports( 'menus' ) );
+		$this->assertTrue( current_theme_supports( 'bu-branding' ) );
+		$this->assertTrue( current_theme_supports( 'bu-profiles-post_type' ) );
+	}
+
+	function test_dynamic_foot_bars() {
+		$this->assertEquals( array(
+			'footbar'           => 'Footbar',
+			'alternate-footbar' => 'Alternate Footbar',
+		), responsive_get_dynamic_footbars() );
 	}
 }
