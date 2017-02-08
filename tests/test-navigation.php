1428238ba27a3f99c31c6113fc03ba42e56e4b02
Jon Desrosiers <desrosj@gmail.com>
2017-02-08 14:44:12 -0700

    Update documentation in tests. Group similar tests into classes and supply a group to PHPUnit.

---
+++ tests/test-navigation.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Test default constants defined in the framework.
+ *
+ * @package Responsive_Framework
+ */
+
+/**
+ * Class Tests_Responsive_Framework_Navigation
+ *
+ * @group navigation
+ */
+class Tests_Responsive_Framework_Navigation extends WP_UnitTestCase {
+	/**
+	 * Setup parent class.
+	 */
+	function setUp() {
+		parent::setUp();
+	}
+
+	/**
+	 * Default nav menu locations.
+	 */
+	function test_nav_menus() {
+		$nav_menus = get_registered_nav_menus();
+
+		$this->assertTrue( isset( $nav_menus['footer'] ) );
+		$this->assertEquals( 'Footer Links', $nav_menus['footer'] );
+
+		$this->assertTrue( isset( $nav_menus['social'] ) );
+		$this->assertEquals( 'Social Links', $nav_menus['social'] );
+
+		$this->assertTrue( isset( $nav_menus['utility'] ) );
+		$this->assertEquals( 'Utility Navigation', $nav_menus['utility'] );
+
+		if ( ! function_exists( 'bu_navigation_display_primary' ) ) {
+			$this->assertTrue( isset( $nav_menus['responsive-primary'] ) );
+			$this->assertEquals( 'Primary Navigation', $nav_menus['responsive-primary'] );
+		}
+	}
+}
