ef878dfac7577136852eac5e924879bc199544cb
ashleykolodziej <kolodz10@bu.edu>
2017-02-22 15:43:29 -0500

    Add main role to templates missing it to fix #146

--- archive.php
+++ archive.php
@@ -8,7 +8,7 @@
 get_header();
 ?>
 
-<div class="content-area">
+<main role="main" class="content-area">
 
 	<?php if ( have_posts() ) : ?>
 
@@ -29,7 +29,7 @@ get_header();
 
 	<?php endif; ?>
 
-</div>
+</main>
 
 <?php r_get_archive_sidebar(); ?>
 
