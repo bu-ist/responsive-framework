8a399ff702037df935c434198803cacf66bf889b
Jon Desrosiers <desrosj@gmail.com>
2017-02-28 11:49:01 -0500

    Add Lightgallery support to default WordPress galleries. Add thumbnail size to improve image quality in galleries. Force this thumbnail size in galleries if thumbnail is selected. Force the images to link to the file.

--- js-dev/galleries.js
+++ js-dev/galleries.js
@@ -1,30 +1,4 @@
 /**
- * Toggle behavior for navigation / search buttons.
- */
-( function ( $ ) {
-	$( '.navToggle' ).on( 'click', function ( e ) {
-		e.preventDefault();
-		$( 'nav, .navToggle' ).toggleClass( 'is-open' );
-		$( '.searchToggle, #quicksearch' ).removeClass( 'is-open' );
-		$( 'body' ).toggleClass( 'nav-open' ).removeClass( 'search-open' );
-	});
-
-	$( '.searchToggle' ).on( 'click', function ( e ) {
-		e.preventDefault();
-		$( 'nav, .navToggle' ).removeClass( 'is-open' );
-
-		if( ! $( this ).hasClass( 'is-open' ) ){
-			setTimeout(function(){
-				$( '#q' ).focus();
-			}, 100 );
-		}
-
-		$( '.searchToggle, #quicksearch' ).toggleClass( 'is-open' );
-		$( 'body' ).toggleClass( 'search-open' ).removeClass('nav-open');
-	});
-} ( jQuery ) );
-
-/**
  * A better WordPress gallery experience for Responsive Framework.
  *
  * @package Resopnsive_Framework
