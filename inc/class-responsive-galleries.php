2c20cc3ebee81493640867034060eccf7fa6e0af
Jon Desrosiers <desrosj@gmail.com>
2017-03-03 15:13:54 -0500

    The scripts.js file is always enqueued from the stylesheet directory. When a child theme is used, the child theme’s version will override the framework’s. Because of this, the gallery JavaScript cannot be in the scripts.js file. Moving it to the js folder allows us to only enqueue the gallery JS when it is needed too.

--- inc/class-responsive-galleries.php
+++ inc/class-responsive-galleries.php
@@ -50,6 +50,8 @@ class Responsive_Galleries {
 
 		wp_enqueue_script( 'lightgallery', get_template_directory_uri() . "/js/vendor/lightgallery/js/lightgallery{$postfix}.js", array( 'jquery' ), '1.3.9', true );
 		wp_enqueue_script( 'lightgallery-thumbnail', get_template_directory_uri() . "/js/vendor/lg-thumbnail/lg-thumbnail{$postfix}..js", array( 'jquery', 'lightgallery' ), '1.0.3', true );
+		wp_enqueue_script( 'responsive-framework-gallery', get_template_directory_uri() . "/js/galleries{$postfix}.js", array( 'jquery', 'lightgallery', 'lightgallery-thumbnail' ), RESPONSIVE_FRAMEWORK_VERSION, true );
+
 		wp_enqueue_style( 'lightgallery', get_template_directory_uri() . '/js/vendor/lightgallery/css/lightgallery.min.css', array(), '1.3.9' );
 	}
 
