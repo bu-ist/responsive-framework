c00c909c678d5afe6eafa69bca0ff54dcd1946d5
Jon Desrosiers <desrosj@gmail.com>
2017-03-06 11:00:05 -0500

    Try not setting require once.

--- tests/test-branding.php
+++ tests/test-branding.php
@@ -22,7 +22,7 @@ class Tests_Responsive_Framework_Branding extends WP_UnitTestCase {
 	 * Theme and framework version constants.
 	 */
 	function test_responsive_branding() {
-		$this->expectOutputString( locate_template( 'template-parts/branding.php', true ) );
+		$this->expectOutputString( locate_template( 'template-parts/branding.php', true, false ) );
 
 		responsive_branding();
 	}
