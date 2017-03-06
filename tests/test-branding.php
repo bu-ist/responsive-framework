729b602706af7c486233f4a36aa75019b1f41cc6
Jon Desrosiers <desrosj@gmail.com>
2017-03-06 11:16:46 -0500

    Try using output buffering and `get_template_part()`

--- tests/test-branding.php
+++ tests/test-branding.php
@@ -22,7 +22,11 @@ class Tests_Responsive_Framework_Branding extends WP_UnitTestCase {
 	 * Theme and framework version constants.
 	 */
 	function test_responsive_branding() {
-		$this->expectOutputString( locate_template( 'template-parts/branding.php', true, false ) );
+		ob_start();
+		get_template_part( 'template-parts/branding.php' );
+		$expected_output = ob_get_clean();
+
+		$this->expectOutputString( $expected_output );
 
 		responsive_branding();
 	}
