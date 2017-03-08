464d38b202294dbaeba1c880428dd621769f203c
ashleykolodziej <kolodz10@bu.edu>
2017-03-08 16:58:45 -0500

    Beginning the post class thing

--- template-parts/content.php
+++ template-parts/content.php
@@ -9,7 +9,7 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>">
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 	<h2>
 		<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_title(); ?></a>
