0f4b7ae2749af8746f071d93b7716f28702ec3ce
ashleykolodziej <kolodz10@bu.edu>
2017-03-17 14:09:06 -0400

    Unique classes to items based on format

--- template-parts/profile-mini.php
+++ template-parts/profile-mini.php
@@ -9,16 +9,14 @@
 
 ?>
 
-<li <?php post_class( 'profile-item' ); ?>>
+<li <?php post_class( 'profile-item profile-item-mini' ); ?>>
 	<?php if ( function_exists( 'bu_thumbnail' ) ) : $thumb_args = array( 'maxwidth' => 150, 'maxheight' => 150 ); ?>
 		<?php bu_thumbnail( '<figure class="profile-photo profile-photo-mini">', '</figure>', $thumb_args ); ?>
 	<?php endif; ?>
-	<div class="details">
-		<?php bu_profile_detail( 'first_name', array( 'before' => '<p class="name">' ) ); ?>
-		<?php bu_profile_detail( 'last_name', array( 'after' => '</p>' ) ); ?>
-		<?php bu_profile_detail( 'email', array( 'format' => 'email', 'before' => '<p class="email">', 'after' => '</p>' ) ); ?>
-		<?php if ( function_exists( 'bu_page_summary' ) ) : ?>
-			<?php bu_page_summary( '<p class="summary">', '</p>' ); ?>
-		<?php endif; ?>
-	</div>
+	<?php bu_profile_detail( 'first_name', array( 'before' => '<p class="profile-name profile-name-mini">' ) ); ?>
+	<?php bu_profile_detail( 'last_name', array( 'after' => '</p>' ) ); ?>
+	<?php bu_profile_detail( 'email', array( 'format' => 'email', 'before' => '<p class="profile-email profile-email-mini">', 'after' => '</p>' ) ); ?>
+	<?php if ( function_exists( 'bu_page_summary' ) ) : ?>
+		<?php bu_page_summary( '<p class="profile-summary profile-summary-mini">', '</p>' ); ?>
+	<?php endif; ?>
 </li>
