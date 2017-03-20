450d27308c604e76d971f961579974a7888470d6
ashleykolodziej <kolodz10@bu.edu>
2017-03-20 15:37:09 -0400

    Escape HTML in url

--- inc/course-feeds.php
+++ inc/course-feeds.php
@@ -16,7 +16,8 @@ function responsive_course_template( $template ) {
 	$template = <<<TPL
 <div class="cf-course">
 	<h4 class="cf-course-title">{{title}}</h4>
-	<p class="meta cf-course-info">{{college}} {{department}} {{course_num}} ({{credits}} credits)</p>
+	<p class="meta cf-course-info">{{college}} {{department}} {{course_num}} ({{credits}} credits). Offered {{offered}}</p>
+	<p class="meta">Prerequisites: <span class="prerequisites">{{prereq_u_plain}}{{prereq_g_plain}}{{coreq_g_plain}}</span></p>
 	<p>{{description}}</p>
 
 	[bu-course-feed_section]
@@ -39,7 +40,7 @@ function responsive_section_template( $template ) {
 	$template = <<<TPL
 <div class="responsive-table cf-section-wrapper">
 <table class="cf-table">
-	<caption>Section {{section_name}}, {{semester}} {{year}} {{date_start}} to {{date_end}} {{year}}</caption>
+	<caption>Section {{section_name}}, {{semester}} {{year}} <span class="cf-section-dates">{{date_start}} to {{date_end}}</span></caption>
 	<thead class="cf-section-header">
 		<tr>
 			<th class="cf-section-instructortitle">Instructor</th>
@@ -74,7 +75,7 @@ function responsive_schedule_template( $template ) {
 	<td class="cf-section-type">{{class_type_text}}</td>
 	<td class="cf-section-day">{{days}}</td>
 	<td class="cf-section-start">{{time_start}}&ndash;{{time_end}}</td>
-	<td class="cf-section-location"><a href="http://www.bu.edu/maps/?search={{building}}">{{building}} {{room}}</a></td>
+	<td class="cf-section-location"><a href="http://www.bu.edu/maps/?search={{{building}}}">{{building}} {{room}}</a></td>
 </tr>
 TPL;
 
