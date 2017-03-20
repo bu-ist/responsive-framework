3f13ebca0301a60f0ce105fc54de0261d695c3d8
ashleykolodziej <kolodz10@bu.edu>
2017-03-20 10:59:26 -0400

    Add unique classes for course feeds and table HTML

--- inc/course-feeds.php
+++ inc/course-feeds.php
@@ -15,8 +15,8 @@
 function responsive_course_template( $template ) {
 	$template = <<<TPL
 <div class="cf-course">
-	<h4>{{title}}</h4>
-	<p class="meta">{{college}} {{department}} {{course_num}} ({{credits}} credits)</p>
+	<h4 class="cf-course-title">{{title}}</h4>
+	<p class="meta cf-course-info">{{college}} {{department}} {{course_num}} ({{credits}} credits)</p>
 	<p>{{description}}</p>
 
 	[bu-course-feed_section]
@@ -38,17 +38,21 @@ add_filter( 'bu_course_feeds_default_course_template', 'responsive_course_templa
 function responsive_section_template( $template ) {
 	$template = <<<TPL
 <p class=""><em>{{section_id}}, {{date_start}} to {{date_end}} {{year}}</em></p>
-<div class="responsive-table">
-<table>
-	<tr>
-		<th>Days</th>
-		<th>Start</th>
-		<th>End</th>
-		<th>Type</th>
-		<th>Bldg</th>
-		<th>Room</th>
-	</tr>
-	[bu-course-feed_schedule]
+<div class="responsive-table cf-section-wrapper">
+<table class="cf-table">
+	<thead class="cf-section-header">
+		<tr>
+			<th class="cf-section-daytitle">Days</th>
+			<th class="cf-section-starttitle">Start</th>
+			<th class="cf-section-endtitle">End</th>
+			<th class="cf-section-typetitle">Type</th>
+			<th class="cf-section-bldgtitle">Bldg</th>
+			<th class="cf-section-roomtitle">Room</th>
+		</tr>
+	</thead>
+	<tbody>
+		[bu-course-feed_schedule]
+	</tbody>
 </table>
 </div>
 TPL;
@@ -66,13 +70,13 @@ add_filter( 'bu_course_feeds_default_section_template', 'responsive_section_temp
  */
 function responsive_schedule_template( $template ) {
 	$template = <<<TPL
-<tr>
-	<td>{{days}}</td>
-	<td>{{time_start}}</td>
-	<td>{{time_end}}</td>
-	<td>{{type}}</td>
-	<td>{{building}}</td>
-	<td>{{room}}</td>
+<tr class="cf-section-item">
+	<td class="cf-section-day">{{days}}</td>
+	<td class="cf-section-start">{{time_start}}</td>
+	<td class="cf-section-end">{{time_end}}</td>
+	<td class="cf-section-type">{{type}}</td>
+	<td class="cf-section-bldg">{{building}}</td>
+	<td class="cf-section-room">{{room}}</td>
 </tr>
 TPL;
 
