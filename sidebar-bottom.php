58348f34d0c38f31ca6b38394e2b36d366d2df0b
Ashley Kolodziej <kolodz10@bu.edu>
2017-07-28 15:09:51 -0400

    Add headings to sidebars for outline

--- sidebar-bottom.php
+++ sidebar-bottom.php
@@ -10,6 +10,7 @@
 $footbar = responsive_get_footbar_id();
 if ( is_active_sidebar( $footbar ) ) : ?>
 <aside class="footbar <?php responsive_sidebar_classes( 'footbar' ); ?>">
+	<h2 class="visually-hidden">More about this site</h2>
 	<div class="footbar-container">
 		<?php dynamic_sidebar( $footbar ); ?>
 	</div>
--- sidebar.php
+++ sidebar.php
@@ -8,6 +8,7 @@
 ?>
 <?php if ( is_active_sidebar( 'sidebar' ) ) : ?>
 	<aside class="sidebar">
+		<h2 class="visually-hidden">Related to <?php the_title(); ?></h2>
 		<?php dynamic_sidebar( 'sidebar' ); ?>
 	</aside>
 <?php endif;
