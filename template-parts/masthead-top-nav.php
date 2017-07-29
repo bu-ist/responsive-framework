10ccbb2f61c3fbe58d3d05185ea6901b26a28c61
Ashley Kolodziej <kolodz10@bu.edu>
2017-07-29 15:29:12 -0400

    Apply accessibility updates to navigation

--- template-parts/masthead-top-nav.php
+++ template-parts/masthead-top-nav.php
@@ -7,10 +7,10 @@
 
 ?>
 <nav class="primary-nav" role="navigation">
-	<a href="#primary-nav-menu" class="nav-toggle js-nav-toggle"><span>Menu</span></a>
+	<button type="button" class="nav-toggle js-nav-toggle" aria-label="Open menu" aria-expanded="true"><span>Menu</span></button>
 
 	<?php if ( responsive_search_is_enabled() ) : ?>
-	<a href="#quicksearch" class="search-toggle js-search-toggle"><span>Search</span></a>
+		<button type="button" class="search-toggle js-search-toggle" aria-label="Open search" aria-expanded="true"><span>Search</span></button>
 	<?php endif; ?>
 
 	<?php responsive_primary_nav(); ?>
