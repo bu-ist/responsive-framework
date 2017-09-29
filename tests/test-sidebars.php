8c3de02e343b1195b311f57e7e446705ff217a1b
Jon Desrosiers <desrosj@gmail.com>
2017-09-29 10:20:00 -0400

    Better test sidebars that get registered based on the presence and value of a constant.

--- tests/test-sidebars.php
+++ tests/test-sidebars.php
@@ -17,17 +17,30 @@ class Tests_Responsive_Framework_Sidebars extends WP_UnitTestCase {
 	function setUp() {
 		parent::setUp();
 
-		define( 'BU_SUPPORTS_DYNAMIC_FOOTBARS', true );
 		responsive_sidebars();
 	}
 
 	/**
-	 * Default sidebars
+	 * Default sidebars.
 	 */
 	function test_registered_sidebars() {
 		$this->assertTrue( is_registered_sidebar( 'sidebar' ) );
 		$this->assertTrue( is_registered_sidebar( 'posts' ) );
 		$this->assertTrue( is_registered_sidebar( 'footbar' ) );
+		$this->assertFalse( is_registered_sidebar( 'alternate-footbar' ) );
+		$this->assertFalse( is_registered_sidebar( 'profiles' ) );
+	}
+
+	/**
+	 * Tests sidebars get registered when the related constants are defined.
+	 */
+	function test_sidebars_requiring_constants() {
+		define( 'BU_SUPPORTS_DYNAMIC_FOOTBARS', true );
+		define( 'BU_PROFILES_PLUGIN_ACTIVE', true );
+
+		responsive_sidebars();
+
 		$this->assertTrue( is_registered_sidebar( 'alternate-footbar' ) );
+		$this->assertTrue( is_registered_sidebar( 'profiles' ) );
 	}
 }
