030acec0439910e6754a91a55808432136179608
Jon Desrosiers <desrosj@gmail.com>
2017-10-02 16:22:17 -0400

    Deactivate BU Mobile when the theme is activated.
    BU Mobile does device checking and passes headers to the client to serve mobile specific pages. Since the theme is responsive by default, we should deactivate BU Mobile when the theme is activated.

---
+++ tests/test-theme-activation.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Test theme activation functionality.
+ *
+ * @package Responsive_Framework
+ */
+
+/**
+ * Class Tests_Responsive_Framework_Activation
+ *
+ * @group sidebars
+ */
+class Tests_Responsive_Framework_Activation extends WP_UnitTestCase {
+
+	function test_bu_mobile_is_disabled() {
+		update_option(
+			'active_plugins',
+			array(
+				'bu-mobile/bu-mobile.php',
+			)
+		);
+
+		responsive_disable_bu_mobile_plugin();
+
+		$this->assertSame( array(), get_option( 'active_plugins' ) );
+	}
+}
