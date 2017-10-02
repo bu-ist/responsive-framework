9ca499a39d610098e0402773eff516dca8d9dc89
Jon Desrosiers <desrosj@gmail.com>
2017-10-02 16:38:27 -0400

    Add missing test method inline documentation.

--- tests/test-theme-activation.php
+++ tests/test-theme-activation.php
@@ -12,6 +12,9 @@
  */
 class Tests_Responsive_Framework_Activation extends WP_UnitTestCase {
 
+	/**
+	 * Test that BU Mobile is deactivated correctly when the theme is activated.
+	 */
 	function test_bu_mobile_is_disabled() {
 		update_option(
 			'active_plugins',
