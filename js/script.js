c140ac1652cfc5a4e87d499c54bbcfa4ea4fb90e
ashleykolodziej <kolodz10@bu.edu>
2017-10-02 17:07:29 -0400

    Rebuild after merge

--- js/script.js
+++ js/script.js
@@ -8,11 +8,9 @@
 		 $searchtoggle = $( '.js-search-toggle' ),
 		 $searchitems = $searchtoggle.add( '#quicksearch' );
 
-	$searchtoggle.attr( 'aria-expanded', 'false' )
-					 .attr( 'aria-controls', 'quicksearch' );
-
-	$toggle.attr( 'aria-expanded', 'false' )
-			 .attr( 'aria-controls', 'primary-nav-menu' );
+	// Add aria attributes for control/expanded if JS is available
+	$searchtoggle.attr( 'aria-expanded', 'false' ).attr( 'aria-controls', 'quicksearch' );
+	$toggle.attr( 'aria-expanded', 'false' ).attr( 'aria-controls', 'primary-nav-menu' );
 
 	$toggle.on( 'click', function ( e ) {
 		e.preventDefault();
@@ -34,17 +32,15 @@
 		$toggleitems.removeClass( 'is-open' );
 
 		if( focus === true && ! $( this ).hasClass( 'is-open' ) ){
-			setTimeout(function(){
+			setTimeout( function(){
 				$( '#q' ).focus();
 			}, 100 );
 		}
 
 		if ( $searchtoggle.attr( 'aria-expanded' ) === 'false' ) {
-			$searchtoggle.attr( 'aria-expanded', 'true' )
-							 .attr( 'aria-label', 'Close search' );
+			$searchtoggle.attr( 'aria-expanded', 'true' ).attr( 'aria-label', 'Close search' );
 		} else {
-			$searchtoggle.attr( 'aria-expanded', 'false' )
-							 .attr( 'aria-label', 'Open search' );
+			$searchtoggle.attr( 'aria-expanded', 'false' ).attr( 'aria-label', 'Open search' );
 		}
 
 		$searchitems.toggleClass( 'is-open' );
@@ -52,12 +48,12 @@
 	}
 
 	$searchtoggle.on({
-		click: function (e) {
+		click: function ( e ) {
 			e.preventDefault();
 			toggleSearchPanel( true );
 		},
-		keypress: function (e) {
-			if ( e.keyCode == 13 ) {
+		keypress: function ( e ) {
+			if ( e.keyCode === 13 ) {
 				e.preventDefault();
 				toggleSearchPanel( false );
 			}
