60e207c08d6e33cf1883a26848bf1db7edbaaedb
Jon Desrosiers <desrosjaccounts@gmail.com>
2017-12-11 10:33:37 -0500

    Remove empty IE conditional before DOCTYPE. Backporting 6a3cbd8 (#273) into `master`.

--- header.php
+++ header.php
@@ -1,4 +1,3 @@
-<!--[if IE ]><![endif]-->
 <!DOCTYPE html>
 <!--[if lt IE 7]>     <html class="no-js ie lt-ie9 lt-ie8 lt-ie7"  lang="en"> <![endif]-->
 <!--[if IE 7]>        <html class="no-js ie lt-ie9 lt-ie8"  lang="en"> <![endif]-->
