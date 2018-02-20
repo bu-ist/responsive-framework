7c35ab8f137a5537c020d8f11b56677f300ded6b
Jon Desrosiers <desrosjaccounts@gmail.com>
2018-02-20 13:32:57 -0500

    Remove `responsive_bu_search_form_query_attributes()`.
    This function is a filter on `bu_search_form_query_attributes` to add the `placeholder` attribute to the search field input. This is now the default behavior in the BU-CMS plugin, making this code redundant and unnecessary.

--- inc/search-form.php
+++ inc/search-form.php
@@ -14,13 +14,3 @@ function responsive_bu_search_form_contexts() {
 	return array( 'site' => 'This Site' );
 }
 add_filter( 'bu_search_form_contexts', 'responsive_bu_search_form_contexts' );
-
-/**
- * Add a placeholder attribute to the search form added by BU CMS.
- *
- * @return string Modified search field input tag attributes.
- */
-function responsive_bu_search_form_query_attributes() {
-	return 'placeholder="Search site&hellip;"';
-}
-add_filter( 'bu_search_form_query_attributes', 'responsive_bu_search_form_query_attributes' );
