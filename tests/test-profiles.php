d7f9dc476511236f725e5d79dee3836a69f5efb6
Jon Desrosiers <desrosjaccounts@gmail.com>
2018-03-19 15:52:58 -0400

    Add tests to ensure profile image sizes are registered.

---
+++ tests/test-profiles.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Test Responsive Framework Profile related functions.
+ *
+ * @package Responsive_Framework
+ */
+
+/**
+ * Class Tests_Responsive_Framework_Profiles
+ *
+ * @group profiles
+ */
+class Tests_Responsive_Framework_Profiles extends WP_UnitTestCase {
+
+	/**
+	 * Test default responsive layout.
+	 */
+	function test_responsive_gallery_after_setup_theme() {
+		BU\Themes\Responsive_Framework\Galleries\after_setup_theme();
+
+		$this->assertTrue( has_image_size( 'responsive_profile' ) );
+		$this->assertTrue( has_image_size( 'responsive_profile_large' ) );
+	}
+
+}
