785ae6ecd9f348ebd8b1e7cd9b25bcb92379dda1
Jon Desrosiers <desrosjaccounts@gmail.com>
2018-03-19 16:05:57 -0400

    Test the correct function.

--- tests/test-profiles.php
+++ tests/test-profiles.php
@@ -16,7 +16,7 @@ class Tests_Responsive_Framework_Profiles extends WP_UnitTestCase {
 	 * Test default responsive layout.
 	 */
 	function test_responsive_gallery_after_setup_theme() {
-		BU\Themes\Responsive_Framework\Galleries\after_setup_theme();
+		BU\Themes\Responsive_Framework\Profiles\after_setup_theme();
 
 		$this->assertTrue( has_image_size( 'responsive_profile' ) );
 		$this->assertTrue( has_image_size( 'responsive_profile_large' ) );
