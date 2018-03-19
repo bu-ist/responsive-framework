d060fd9058c31b1f0bb89fbc0311326f327c8dd5
Jon Desrosiers <desrosjaccounts@gmail.com>
2018-03-19 15:50:14 -0400

    Use `r_get_tempalte_part()` on search templates.

--- search.php
+++ search.php
@@ -20,7 +20,7 @@ get_header(); ?>
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content', 'search' ); ?>
+				<?php r_get_template_part( get_post_type(), 'search' ); ?>
 
 			<?php endwhile; ?>
 
