637a3f015e841d9ee90e932be42a936bba4d5be2
Jon Desrosiers <desrosjaccounts@gmail.com>
2018-03-26 15:29:11 -0400

    Add action hooks to sidebar templates.

--- sidebar-calendar.php
+++ sidebar-calendar.php
@@ -5,7 +5,28 @@
  * @package Responsive_Framework
  */
 
+/**
+ * Fires immediately before the opening calendar sidebar container element.
+ */
+do_action( 'r_before_sidebar_calendar_opening' );
 ?>
 <aside class="sidebar sidebar-calendar">
-	<?php responsive_calendar_sidebar(); ?>
+<?php
+	/**
+	 * Fires immediately after the opening calendar sidebar container element.
+	 */
+	do_action( 'r_after_sidebar_calendar_opening' );
+
+	responsive_calendar_sidebar();
+
+	/**
+	 * Fires immediately before the closing calendar sidebar container element.
+	 */
+	do_action( 'r_before_sidebar_calendar_closing' );
+?>
 </aside>
+<?php
+/**
+ * Fires immediately after the closing calendar sidebar container element.
+ */
+do_action( 'r_after_sidebar_calendar_closing' );
--- sidebar-posts.php
+++ sidebar-posts.php
@@ -5,9 +5,33 @@
  * @package Responsive_Framework
  */
 
-?>
-<?php if ( is_active_sidebar( 'posts' ) ) : ?>
+
+if ( is_active_sidebar( 'posts' ) ) :
+
+	/**
+	 * Fires immediately before the opening posts sidebar container element.
+	 */
+	do_action( 'r_before_sidebar_posts_opening' );
+	?>
 	<aside class="sidebar sidebar-posts">
-		<?php dynamic_sidebar( 'posts' ); ?>
+	<?php
+		/**
+		 * Fires immediately after the opening posts sidebar container element.
+		 */
+		do_action( 'r_after_sidebar_posts_opening' );
+
+		dynamic_sidebar( 'posts' );
+
+		/**
+		 * Fires immediately before the closing posts sidebar container element.
+		 */
+		do_action( 'r_before_sidebar_posts_closing' );
+	?>
 	</aside>
-<?php endif;
+	<?php
+	/**
+	 * Fires immediately after the closing posts sidebar container element.
+	 */
+	do_action( 'r_after_sidebar_posts_closing' );
+
+endif;
