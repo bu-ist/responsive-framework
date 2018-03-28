99f6ebe6ff032353aaf02f4b40708b8be0f17f2b
Jon Desrosiers <desrosjaccounts@gmail.com>
2018-03-28 11:47:04 -0400

    Move the location descriptor for each sidebar action hook to the end.
    This makes the first part of the action hooks consistent for every related hook.

--- sidebar-posts.php
+++ sidebar-posts.php
@@ -10,27 +10,27 @@ if ( is_active_sidebar( 'posts' ) ) :
 	/**
 	 * Fires immediately before the opening posts sidebar container element.
 	 */
-	do_action( 'r_before_sidebar_posts_opening' );
+	do_action( 'r_sidebar_posts_opening_before' );
 	?>
 	<aside class="sidebar sidebar-posts">
 	<?php
 		/**
 		 * Fires immediately after the opening posts sidebar container element.
 		 */
-		do_action( 'r_after_sidebar_posts_opening' );
+		do_action( 'r_sidebar_posts_opening_after' );
 
 		dynamic_sidebar( 'posts' );
 
 		/**
 		 * Fires immediately before the closing posts sidebar container element.
 		 */
-		do_action( 'r_before_sidebar_posts_closing' );
+		do_action( 'r_sidebar_posts_closing_before' );
 	?>
 	</aside>
 	<?php
 	/**
 	 * Fires immediately after the closing posts sidebar container element.
 	 */
-	do_action( 'r_after_sidebar_posts_closing' );
+	do_action( 'r_sidebar_posts_closing_after' );
 
 endif;
--- sidebar-profiles.php
+++ sidebar-profiles.php
@@ -9,27 +9,27 @@ if ( is_active_sidebar( 'profiles' ) ) :
 	/**
 	 * Fires immediately before the opening profiles sidebar container element.
 	 */
-	do_action( 'r_before_sidebar_profiles_opening' );
+	do_action( 'r_sidebar_profiles_opening_before' );
 	?>
 	<aside class="sidebar sidebar-profiles">
 	<?php
 		/**
 		 * Fires immediately after the opening profiles sidebar container element.
 		 */
-		do_action( 'r_after_sidebar_profiles_opening' );
+		do_action( 'r_sidebar_profiles_opening_after' );
 
 		dynamic_sidebar( 'profiles' );
 
 		/**
 		 * Fires immediately before the closing profiles sidebar container element.
 		 */
-		do_action( 'r_before_sidebar_profiles_closing' );
+		do_action( 'r_sidebar_profiles_closing_before' );
 	?>
 	</aside>
 	<?php
 	/**
 	 * Fires immediately after the closing profiles sidebar container element.
 	 */
-	do_action( 'r_after_sidebar_profiles_closing' );
+	do_action( 'r_sidebar_profiles_closing_after' );
 
 endif;
