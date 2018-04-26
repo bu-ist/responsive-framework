89168d9097ef5fddfc10f39444ee946b4e1e0de8
Jon Desrosiers <desrosj@gmail.com>
2018-04-26 12:49:09 -0400

    Prevent the news template from triggering a fatal error when `BU_News_Page_Template` does not exist.

--- page-templates/news.php
+++ page-templates/news.php
@@ -5,9 +5,11 @@
  * @package Responsive_Framework
  */
 
-// Setup for secondary query that lists posts based on the page's metadata.
-include BU_NEWS_POST_LISTS_PATH . '/news-page-template.php';
-BU_News_Page_Template::init();
+if ( class_exists( 'BU_News_Page_Template' ) ) {
+	// Setup for secondary query that lists posts based on the page's metadata.
+	include BU_NEWS_POST_LISTS_PATH . '/news-page-template.php';
+	BU_News_Page_Template::init();
+}
 
 get_header();
 ?>
@@ -16,38 +18,42 @@ get_header();
 
 	<article id="post-<?php the_ID(); ?>" <?php post_class( 'content-area' ); ?>>
 
-	<?php the_title( '<h1 class="page-title">', '</h1>' ); ?>
+		<?php the_title( '<h1 class="page-title">', '</h1>' ); ?>
 
-	<?php the_content(); ?>
+		<?php the_content(); ?>
 
-	<?php
-		wp_link_pages( array(
-			'before' => sprintf( '<div class="page-link">%s', esc_html__( 'Pages:', 'responsive-framework' ) ),
-			'after' => '</div>',
-		) );
-	?>
+		<?php
+			wp_link_pages( array(
+				'before' => sprintf( '<div class="page-link">%s', esc_html__( 'Pages:', 'responsive-framework' ) ),
+				'after' => '</div>',
+			) );
+		?>
 
-	<?php edit_post_link( __( 'Edit', 'responsive-framework' ), '<span class="edit-link">', '</span>' ); ?>
+		<?php edit_post_link( __( 'Edit', 'responsive-framework' ), '<span class="edit-link">', '</span>' ); ?>
 
-	<?php $news_query = BU_News_Page_Template::query_posts(); ?>
+		<?php if ( class_exists( 'BU_News_Page_Template' ) ) : ?>
 
-		<?php if ( $news_query->have_posts() ) : ?>
-		<section class="news-posts">
+			<?php $news_query = BU_News_Page_Template::query_posts(); ?>
 
-		<?php while ( $news_query->have_posts() ) : $news_query->the_post(); ?>
-			<?php get_template_part( 'template-parts/news', 'archive' ); ?>
-		<?php endwhile; ?>
+				<?php if ( $news_query->have_posts() ) : ?>
+				<section class="news-posts">
 
-		<?php responsive_posts_navigation( null, $news_query ); ?>
+				<?php while ( $news_query->have_posts() ) : $news_query->the_post(); ?>
+					<?php get_template_part( 'template-parts/news', 'archive' ); ?>
+				<?php endwhile; ?>
 
-		</section>
+				<?php responsive_posts_navigation( null, $news_query ); ?>
 
-	<?php else : ?>
+				</section>
 
-		<h1><?php esc_html_e( 'No Posts Found', 'responsive-framework' ); ?></h1>
-		<p><?php esc_html_e( 'This site does not currently have any posts. Please check back later.', 'responsive-framework' ); ?></p>
+			<?php else : ?>
 
-	<?php endif; ?>
+				<h1><?php esc_html_e( 'No Posts Found', 'responsive-framework' ); ?></h1>
+				<p><?php esc_html_e( 'This site does not currently have any posts. Please check back later.', 'responsive-framework' ); ?></p>
+
+			<?php endif; ?>
+
+		<?php endif; ?>
 
 		<?php wp_reset_postdata(); ?>
 
