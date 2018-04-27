1f7237ea4ad7fc5e651ae16cdd2f201375c9ac27
Jon Desrosiers <desrosj@gmail.com>
2018-04-27 13:19:48 -0400

    Add api-gen to the list of Composer packages and set up GH Pages deployment.

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,4 +6,11 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'TokenReflection' => array($vendorDir . '/andrewsville/php-token-reflection'),
+    'Symfony\\Component\\OptionsResolver\\' => array($vendorDir . '/symfony/options-resolver'),
+    'Kdyby\\Events\\' => array($vendorDir . '/kdyby/events/src'),
+    'Herrera\\Version' => array($vendorDir . '/herrera-io/version/src/lib'),
+    'Herrera\\Phar\\Update' => array($vendorDir . '/herrera-io/phar-update/src/lib'),
+    'Herrera\\Json' => array($vendorDir . '/herrera-io/json/src/lib'),
+    'FSHL' => array($vendorDir . '/kukulich/fshl'),
 );
