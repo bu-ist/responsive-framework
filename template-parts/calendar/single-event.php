49e129000f92670ec7cef2397b2f4fefd118bec1
Todd Milliken <milliktr@gmail.com>
2018-07-23 15:33:55 -0400

    Add filter for hardcoded words on calendar event
    Adds a filter to allow devs to change all the hardcoded words in the single-event view. This could be done by adding something like:
    
    ```
    /**
     * Modifies responsive-framework single-event hardcoded strings.
     *
     * @param array $words Array of words used on the BU calendar template for single-events.
     * @return array $words
     */
    function modify_words( $words ) {
    	$words['registration'] = __( 'Register Now:', 'my-child-theme' );
    	return $words;
    }
    add_filter( 'responsive_calendar_event_strings', 'modify_words' );
    ```
    
    Fixed some invalid HTML like replacing a closing </dt> tag with an </li> tag, or closing a div that was left open.
    
    Changed if statements in templating to use the written out syntax with colon rather than brackets for better legibility with comingled HTML.

--- template-parts/calendar/single-event.php
+++ template-parts/calendar/single-event.php
@@ -7,6 +7,32 @@
 
 global $buCalendar;
 
+$words = apply_filters( 'responsive_calendar_event_strings', array(
+	'address'              => __( 'Address', 'responsive-framework' ) . ':',
+	'audience'             => __( 'Audience', 'responsive-framework' ) . ':',
+	'back_to_calendar'     => __( 'Back to Calendar', 'responsive-framework' ),
+	'contact_email'        => __( 'Contact Email', 'responsive-framework' ) . ':',
+	'contact_name'         => __( 'Contact Name', 'responsive-framework' ) . ':',
+	'contact_organization' => __( 'Contact Organization', 'responsive-framework' ) . ':',
+	'contact_phone'        => __( 'Contact Phone', 'responsive-framework' ) . ':',
+	'deadline'             => __( 'Deadline', 'responsive-framework' ) . ':',
+	'departments'          => __( 'Departments', 'responsive-framework' ) . ':',
+	'ends'                 => __( 'Ends', 'responsive-framework' ) . ':',
+	'fee'                  => __( 'Fees', 'responsive-framework' ) . ':',
+	'fee_general'          => __( 'Fee (General)', 'responsive-framework' ) . ':',
+	'fee_public'           => __( 'Fee (Public)', 'responsive-framework' ) . ':',
+	'fee_senior'           => __( 'Fee (Seniors)', 'responsive-framework' ) . ':',
+	'fee_staff'            => __( 'Fee (Staff)', 'responsive-framework' ) . ':',
+	'fee_student'          => __( 'Fee (Students)', 'responsive-framework' ) . ':',
+	'fee_student_bu'       => __( 'Fee (BU Students)', 'responsive-framework' ) . ':',
+	'fees'                 => __( 'Fees', 'responsive-framework' ) . ':',
+	'location'             => __( 'Location', 'responsive-framework' ) . ':',
+	'registration'         => __( 'Registration', 'responsive-framework' ) . ':',
+	'room'                 => __( 'Room', 'responsive-framework' ) . ':',
+	'speakers'             => __( 'Speakers', 'responsive-framework' ) . ':',
+	'starts'               => __( 'Starts', 'responsive-framework' ) . ':',
+) );
+
 // Retrieve single-event parameters.
 $calendar_id = responsive_calendar_get_calendar_id();
 $event_id    = responsive_calendar_get_event_id();
@@ -20,12 +46,15 @@ $event = $buCalendar->getEvent( $calendar_id, $event_id, $oid );
 		<h1><?php echo wp_kses_post( $event['summary'] ); ?></h1>
 		<div class="single-event-schedule">
 			<ul class="single-event-schedule-list">
-				<?php if ( ! empty( $event['start_time'] ) ) : ?>
+				<?php if ( ! empty( $event['start_time'] ) && ! empty( $words['starts'] ) ) : ?>
 					<li class="single-event-schedule-start">
-						<span class="single-event-label">Starts: </span><?php printf( '<span class="single-event-time">%s</span><em class="event-time-make-sentence"> on </em><span class="single-event-date">%s</span>', esc_html( date( 'g:i a', $event['starts'] ) ), esc_html( date( 'l, F j, Y', $event['starts'] ) ) ); ?>
+						<span class="single-event-label"><?php echo esc_html( $words['starts'] ); ?> </span>
+						<?php printf( '<span class="single-event-time">%s</span><em class="event-time-make-sentence"> on </em><span class="single-event-date">%s</span>', esc_html( date( 'g:i a', $event['starts'] ) ), esc_html( date( 'l, F j, Y', $event['starts'] ) ) ); ?>
 					</li>
-					<?php if ( $event['ends'] > 0 ) : ?>
-						<li class="single-event-schedule-end"><span class="single-event-label">Ends: </span><?php printf( '<span class="single-event-time">%s</span><em class="event-time-make-sentence"> on </em><span class="single-event-date">%s</span>', esc_html( date( 'g:i a', $event['ends'] ) ), esc_html( date( 'l, F j, Y', $event['ends'] ) ) ); ?></dd>
+					<?php if ( ( $event['ends'] > 0 ) && ! empty( $words['ends'] ) ) : ?>
+						<li class="single-event-schedule-end">
+							<span class="single-event-label"><?php echo esc_html( $words['ends'] ); ?> </span>
+							<?php printf( '<span class="single-event-time">%s</span><em class="event-time-make-sentence"> on </em><span class="single-event-date">%s</span>', esc_html( date( 'g:i a', $event['ends'] ) ), esc_html( date( 'l, F j, Y', $event['ends'] ) ) ); ?></dd>
 					<?php endif; ?>
 				<?php else : ?>
 					<?php printf( '<li class="single-event-schedule-allday"><span class="single-event-label">All Day</span><em> on </em><span class="single-event-date">%s</span></li>', esc_html( date( 'l, F j, Y', $event['starts'] - intval( date( 'Z' ) ) ) ) ); ?>
@@ -37,98 +66,93 @@ $event = $buCalendar->getEvent( $calendar_id, $event_id, $oid );
 
 	<div class="single-event-additional-details">
 		<dl class="tabular">
-			<?php if ( $event['speakers'] ) { ?>
-				<dt class="single-event-speakers-label">Speakers:</dt>
+			<?php if ( $event['speakers'] && ! empty( $words['speakers'] ) ) : ?>
+				<dt class="single-event-speakers-label"><?php echo esc_html( $words['speakers'] ); ?></dt>
 				<dd class="single-event-speakers-info"><?php echo wp_kses_post( $event['speakers'] ); ?></dd>
-			<?php } ?>
-			<?php if ( $event['audience'] ) { ?>
-				<dt class="single-event-audience-label">Audience:</dt>
+			<?php endif; ?>
+			<?php if ( $event['audience'] && ! empty( $words['audience'] ) ) : ?>
+				<dt class="single-event-audience-label"><?php echo esc_html( $words['audience'] ); ?></dt>
 				<dd class="single-event-audience-info"><?php echo wp_kses_post( $event['audience'] ); ?></dd>
-			<?php } ?>
-			<?php if ( $event['departments'] ) { ?>
-				<dt class="single-event-departments-label">Departments:</dt>
+			<?php endif; ?>
+			<?php if ( $event['departments'] && ! empty( $words['departments'] ) ) : ?>
+				<dt class="single-event-departments-label"><?php echo esc_html( $words['departments'] ); ?></dt>
 				<dd class="single-event-departments-info"><?php echo wp_kses_post( $event['departments'] ); ?></dd>
-			<?php } ?>
-			<?php if ( $event['location'] ) { ?>
-				<dt class="single-event-location-label">Location:</dt>
+			<?php endif; ?>
+			<?php if ( $event['location'] && ! empty( $words['location'] ) ) : ?>
+				<dt class="single-event-location-label"><?php echo esc_html( $words['location'] ); ?></dt>
 				<dd class="single-event-location-info"><?php echo wp_kses_post( $event['location'] ); ?></dd>
-			<?php } ?>
-			<?php if ( $event['locationBuilding'] ) { ?>
-				<dt class="single-event-location-building-label">Address:</dt>
+			<?php endif; ?>
+			<?php if ( $event['locationBuilding'] && ! empty( $words['address'] ) ) : ?>
+				<dt class="single-event-location-building-label"><?php echo esc_html( $words['address'] ); ?></dt>
 				<dd class="single-event-location-building-info"><?php echo wp_kses_post( $event['locationBuilding'] ); ?></dd>
-			<?php } ?>
-			<?php if ( $event['locationRoom'] ) { ?>
-				<dt class="single-event-location-room-label">Room:</dt>
+			<?php endif; ?>
+			<?php if ( $event['locationRoom'] && ! empty( $words['room'] ) ) : ?>
+				<dt class="single-event-location-room-label"><?php echo esc_html( $words['room'] ); ?></dt>
 				<dd class="single-event-location-room-info"><?php echo wp_kses_post( $event['locationRoom'] ); ?></dd>
-			<?php } ?>
-			<?php if ( $event['fees'] ) { ?>
-				<dt class="single-event-fees-label">Fees:</dt>
+			<?php endif; ?>
+			<?php if ( $event['fees'] && ! empty( $words['fees'] ) ) : ?>
+				<dt class="single-event-fees-label"><?php echo esc_html( $words['fees'] ); ?></dt>
 				<dd class="single-event-fees-info"><?php echo wp_kses_post( $event['fees'] ); ?></dd>
-			<?php } ?>
-			<?php if ( $event['fee'] ) { ?>
-				<dt class="single-event-fee-label">Fees:</dt>
+			<?php endif; ?>
+			<?php if ( $event['fee'] && ! empty( $words['fee'] ) ) : ?>
+				<dt class="single-event-fee-label"><?php echo esc_html( $words['fee'] ); ?></dt>
 				<dd class="single-event-fee-info"><?php echo wp_kses_post( $event['fee'] ); ?></dd>
-			<?php } ?>
-			<?php if ( $event['feeGeneral'] ) { ?>
-				<dt class="single-event-fee-general-label">Fee (General):</dt>
+			<?php endif; ?>
+			<?php if ( $event['feeGeneral'] && ! empty( $words['fee_general'] ) ) : ?>
+				<dt class="single-event-fee-general-label"><?php echo esc_html( $words['fee_general'] ); ?></dt>
 				<dd class="single-event-fee-general-info"><?php echo wp_kses_post( $event['feeGeneral'] ); ?></dd>
-			<?php } ?>
-			<?php if ( $event['feePublic'] ) { ?>
-				<dt class="single-event-fee-public-label">Fee (Public):</dt>
+			<?php endif; ?>
+			<?php if ( $event['feePublic'] && ! empty( $words['fee_public'] ) ) : ?>
+				<dt class="single-event-fee-public-label"><?php echo esc_html( $words['fee_public'] ); ?></dt>
 				<dd class="single-event-fee-public-info"><?php echo wp_kses_post( $event['feePublic'] ); ?></dd>
-			<?php } ?>
-			<?php if ( $event['feeStaff'] ) { ?>
-				<dt class="single-event-fee-staff-label">Fee (Staff):</dt>
+			<?php endif; ?>
+			<?php if ( $event['feeStaff'] && ! empty( $words['fee_staff'] ) ) : ?>
+				<dt class="single-event-fee-staff-label"><?php echo esc_html( $words['fee_staff'] ); ?></dt>
 				<dd class="single-event-fee-staff-info"><?php echo wp_kses_post( $event['feeStaff'] ); ?></dd>
-			<?php } ?>
-			<?php if ( $event['feeStudent'] ) { ?>
-				<dt class="single-event-fee-student-label">Fee (Students):</dt>
+			<?php endif; ?>
+			<?php if ( $event['feeStudent'] && ! empty( $words['fee_student'] ) ) : ?>
+				<dt class="single-event-fee-student-label"><?php echo esc_html( $words['fee_student'] ); ?></dt>
 				<dd class="single-event-fee-student-info"><?php echo wp_kses_post( $event['feeStudent'] ); ?></dd>
-			<?php } ?>
-			<?php if ( $event['feeBUStudent'] ) { ?>
-				<dt class="single-event-fee-bu-student-label">Fee (BU Students):</dt>
+			<?php endif; ?>
+			<?php if ( $event['feeBUStudent'] && ! empty( $words['fee_student_bu'] ) ) : ?>
+				<dt class="single-event-fee-bu-student-label"><?php echo esc_html( $words['fee_student_bu'] ); ?></dt>
 				<dd class="single-event-fee-bu-student-info"><?php echo wp_kses_post( $event['feeBUStudent'] ); ?></dd>
-			<?php } ?>
-			<?php if ( $event['feeSenior'] ) { ?>
-				<dt class="single-event-fee-senior-label">Fee (Seniors):</dt>
+			<?php endif; ?>
+			<?php if ( $event['feeSenior'] && ! empty( $words['fee_senior'] ) ) : ?>
+				<dt class="single-event-fee-senior-label"><?php echo esc_html( $words['fee_senior'] ); ?></dt>
 				<dd class="single-event-fee-senior-info"><?php echo wp_kses_post( $event['feeSenior'] ); ?></dd>
-			<?php } ?>
-			<?php if ( $event['deadline'] ) { ?>
-				<dt class="single-event-deadline-label">Deadline:</dt>
+			<?php endif; ?>
+			<?php if ( $event['deadline'] && ! empty( $words['deadline'] ) ) : ?>
+				<dt class="single-event-deadline-label"><?php echo esc_html( $words['deadline'] ); ?></dt>
 				<dd class="single-event-deadline-info"><?php echo wp_kses_post( $event['deadline'] ); ?></dd>
-			<?php } ?>
-			<?php
-			if ( $event['url'] ) {
-				$url_text = $event['url'];
-				if ( $event['urlText'] ) {
-					$url_text = $event['urlText'];
-				}
-				?>
-					<dt class="single-event-registration-label">Registration:</dt>
-					<dd class="single-event-registration-info"><?php printf( '<a href="%s" class="single-event-registration-link">%s</a>', esc_url( $event['url'] ), esc_html( $url_text ) ); ?></dd>
-				<?php
-			}
-			?>
-			<?php if ( $event['contactOrganization'] ) { ?>
-				<dt class="single-event-contact-org-label">Contact Organization:</dt>
+			<?php endif; ?>
+			<?php if ( $event['url'] && ! empty( $words['registration'] ) ) : ?>
+				<?php $url_text = $event['urlText'] ? $event['urlText'] : $event['url']; ?>
+				<dt class="single-event-registration-label"><?php echo esc_html( $words['registration'] ); ?></dt>
+				<dd class="single-event-registration-info"><?php printf( '<a href="%s" class="single-event-registration-link">%s</a>', esc_url( $event['url'] ), esc_html( $url_text ) ); ?></dd>
+			<?php endif; ?>
+			<?php if ( $event['contactOrganization'] && ! empty( $words['contact_organization'] ) ) : ?>
+				<dt class="single-event-contact-org-label"><?php echo esc_html( $words['contact_organization'] ); ?></dt>
 				<dd class="single-event-contact-org-info"><?php echo wp_kses_post( $event['contactOrganization'] ); ?></dd>
-			<?php } ?>
-			<?php if ( $event['contact_name'] ) { ?>
-				<dt class="single-event-contact-name-label">Contact Name:</dt>
+			<?php endif; ?>
+			<?php if ( $event['contact_name'] && ! empty( $words['contact_name'] ) ) : ?>
+				<dt class="single-event-contact-name-label"><?php echo esc_html( $words['contact_name'] ); ?></dt>
 				<dd class="single-event-contact-name-info"><?php echo wp_kses_post( $event['contact_name'] ); ?></dd>
-			<?php } ?>
-			<?php if ( $event['contact_email'] ) { ?>
-				<dt class="single-event-contact-email-label">Contact Email:</dt>
+			<?php endif; ?>
+			<?php if ( $event['contact_email'] && ! empty( $words['contact_email'] ) ) : ?>
+				<dt class="single-event-contact-email-label"><?php echo esc_html( $words['contact_email'] ); ?></dt>
 				<dd class="single-event-contact-email-info"><?php printf( '<a href="mailto:%s" class="single-event-contact-email-link">%s</a>', esc_url( $event['contact_email'] ), esc_html( $event['contact_email'] ) ); ?></dd>
-			<?php } ?>
-			<?php if ( $event['phone'] ) { ?>
-				<dt class="single-event-contact-phone-label">Contact Phone:</dt>
+			<?php endif; ?>
+			<?php if ( $event['phone'] && ! empty( $words['contact_phone'] ) ) : ?>
+				<dt class="single-event-contact-phone-label"><?php echo esc_html( $words['contact_phone'] ); ?></dt>
 				<dd class="single-event-contact-phone-info"><?php echo wp_kses_post( $event['phone'] ); ?></dd>
-			<?php } ?>
+			<?php endif; ?>
 
 		</dl>
 	</div> <!-- /.additionalDetails -->
 
-	<p class="archive-link-container"><a href="<?php the_permalink(); ?>" class="archive-link calendar-archive-link">Back to Calendar</a></p>
+	<?php if ( ! empty( $words['back_to_calendar'] ) ) : ?>
+		<p class="archive-link-container"><a href="<?php the_permalink(); ?>" class="archive-link calendar-archive-link"><?php echo esc_html( $words['back_to_calendar'] ); ?></a></p>
+	<?php endif; ?>
 
 </div><!-- /.single-event -->
