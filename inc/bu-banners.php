a2bd9d38fa15eedce95017d15d3c088434b2e8ee
Todd Milliken <milliktr@gmail.com>
2018-07-31 10:31:35 -0400

    Add comments and doc blocks to filters

--- inc/bu-banners.php
+++ inc/bu-banners.php
@@ -8,13 +8,18 @@
  */
 
 if ( ! function_exists( 'responsive_bu_banner_title' ) ) {
+
 	/**
-	 * Sets the page title in banners if banner title is empty.
+	 * Sets the banner title to the current page title if empty.
 	 *
-	 * If banner exists, and the banner title is empty, adds the page title
+	 * If a banner exists, and the banner title is empty, adds the page title
 	 * to the banner and removes it from the general page content area.
 	 *
-	 * @since    2.1.5
+	 * - Hooks into BU Banners plugin filter `bu_banners_banner_info` to modify banner values.
+	 * - Hooks into Responsive Framework theme filter `responsive_filter_the_title` to set an
+	 *   empty page title since BU Banners will have it.
+	 *
+	 * @since 2.1.5
 	 */
 	function responsive_bu_banner_title() {
 
@@ -29,7 +34,17 @@ if ( ! function_exists( 'responsive_bu_banner_title' ) ) {
 		// Only continues if we have a bu banner, its content is not empty, and there is no title field supplied.
 		if ( bu_has_banner() && ! empty( $banner_content[0] ) && empty( $banner_content[0]['title'] ) ) {
 
-			// Adds the H1 and current page title to banner.
+			/**
+			 * Filters BU Banner values.
+			 *
+			 * Sets banner title to the current page title.
+			 * Sets banner heading tags to use a heading 1 element.
+			 *
+			 * @since 2.1.5
+			 *
+			 * @param  array $banner_info Banner content values.
+			 * @return array $banner_info
+			 */
 			add_filter( 'bu_banners_banner_info', function( $banner_info ) {
 				$banner_info['title']        = get_the_title();
 				$banner_info['title_before'] = '<h1 class="page-title bu-banner-title">';
@@ -37,7 +52,16 @@ if ( ! function_exists( 'responsive_bu_banner_title' ) ) {
 				return $banner_info;
 			} );
 
-			// Removes the H1 and current page title from theme.
+			/**
+			 * Filters Responsive Framework page title.
+			 *
+			 * Removes the H1 that would normally appear and current page title from theme.
+			 *
+			 * @since 2.1.5
+			 *
+			 * @param  array $title_args Page title arguments, same as `the_title()`.
+			 * @return array $title_args
+			 */
 			add_filter( 'responsive_filter_the_title', function( $title_args ) {
 				$title_args['title'] = false;
 				return $title_args;
