68a6e5d6d540631d7c108b588fae3e80e86281e3
Alana Martin <alana@stonybrookweb.com>
2018-09-28 13:24:14 -0400

    clarify comments, remove unused $key variable,

--- functions.php
+++ functions.php
@@ -719,24 +719,26 @@ function r_is_bu_text_widget_empty( $is_widget_empty, $params ) {
 	$ancestor_ids = get_post_ancestors( get_the_ID() );
 
 	if ( 'BU Text' === $widget_name ) {
-		$widget_instance  = $params[1]['number'];
-		$meta_key         = '_bu_text_widget_' . $widget_instance;
-		$widget_meta      = get_post_meta( get_the_ID(), $meta_key, true );
-		$show_on_children = '_bu_text_widget_show_on_children_' . $widget_instance;
+		$widget_instance        = $params[1]['number'];
+		$meta_key               = '_bu_text_widget_' . $widget_instance;
+		$widget_meta            = get_post_meta( get_the_ID(), $meta_key, true );
+		$show_children_meta_key = '_bu_text_widget_show_on_children_' . $widget_instance;
 
 		if ( empty( $widget_meta['content'] ) ) {
-			$is_widget_empty = true; // Assume the widget is empty unless we prove otherwise.
+			// The widget is empty. Assume we don't have ancestor widgets to
+			// show unless we prove otherwise.
+			$is_widget_empty = true;
 
 			if ( ! empty( $ancestor_ids ) ) {
-				foreach ( $ancestor_ids as $key => $ancestor_id ) {
+				foreach ( $ancestor_ids as $ancestor_id ) {
 					// Check to see if widget exists on ancestors and is set to show children.
-					$parent_widget_meta    = get_post_meta( $ancestor_id, $meta_key, true );
-					$parent_widget_show_on = get_post_meta( $ancestor_id, $show_on_children, true );
-					if ( 'Yes' === $parent_widget_show_on ) {
+					$parent_widget_meta = get_post_meta( $ancestor_id, $meta_key, true );
+					$show_on_children   = get_post_meta( $ancestor_id, $show_children_meta_key, true );
+					if ( 'Yes' === $show_on_children ) {
 						if ( ! empty( $parent_widget_meta['content'] ) ) {
 							// This matches the way the plugin currently works.
 							// We only go to the 1st parent that says `Yes` show
-							// on children, regardless if content is available.
+							// on children, regardless if content is empty.
 							$is_widget_empty = false;
 							break;
 						}
@@ -772,7 +774,6 @@ add_action( 'after_setup_theme', 'r_bu_text_widget_loaded' );
  * @return bool $is_widget_empty The status of content for the widget.
  */
 function r_is_bu_links_widget_empty( $is_widget_empty, $params ) {
-
 	$widget_name = $params[0]['widget_name'];
 
 	if ( 'BU Links' === $widget_name ) {
@@ -782,12 +783,13 @@ function r_is_bu_links_widget_empty( $is_widget_empty, $params ) {
 		$ancestor_ids    = get_post_ancestors( get_the_ID() );
 
 		if ( ! is_array( $widget_meta ) ) {
-			// The post has no parents so we can safely say the widget is empty.
 			if ( empty( $ancestor_ids ) ) {
+				// The post has no parents so we can safely say the widget is empty.
 				$is_widget_empty = true;
 			} else {
-				$is_widget_empty = true; // Assume the widget is empty unless we prove otherwise.
-				foreach ( $ancestor_ids as $key => $ancestor_id ) {
+				// Assume no parent widgets to display unless we prove otherwise.
+				$is_widget_empty = true;
+				foreach ( $ancestor_ids as $ancestor_id ) {
 					// Check to see if widget exists on each parent and is set to show children.
 					$parent_widget_meta = get_post_meta( $ancestor_id, $meta_key, true );
 					if ( is_array( $parent_widget_meta ) && array_key_exists( 'show', $parent_widget_meta ) ) {
