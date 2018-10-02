cf5fcb70def7807a559ee8ba587fb7dbf485e404
Alana Martin <alana@stonybrookweb.com>
2018-10-02 14:31:02 -0400

    Update Travis version of WordPress Version to Test to match our current version. Remove test code related to older versions of WP. Update is_responsive_bu_domain tests  to use different test for multi vs single site.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -28,18 +28,6 @@ function _register_theme() {
 	add_filter( 'pre_option_stylesheet', function() use ( $current_theme ) {
 		return $current_theme;
 	});
-
-	/**
-	 * Before WordPress version 4.7, the theme's functions.php file needs to be included manually.
-	 * This only needs to be done when running as multisite.
-	 */
-	if ( ! function_exists( 'responsive_init' ) && ! is_multisite() ) {
-		global $wp_version;
-
-		if ( 1 > version_compare( '4.7', $wp_version ) ) {
-			require_once( dirname( dirname( __FILE__ ) ) . '/functions.php' );
-		}
-	}
 }
 tests_add_filter( 'muplugins_loaded', '_register_theme' );
 
