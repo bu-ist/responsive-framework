7cbcdc1fd7382acf1251a11285fa75ccb2b80f98
ashleykolodziej <kolodz10@bu.edu>
2018-12-03 16:50:18 -0500

    Move short nav

--- template-parts/masthead-mega-nav.php
+++ template-parts/masthead-mega-nav.php
@@ -12,12 +12,12 @@
 <div class="brand">
 	<?php responsive_branding(); ?>
 	<p class="site-description brand-site-description"><?php bloginfo( 'description' ); ?></p>
+	<?php responsive_short_nav(); ?>
 </div>
 
 <nav class="primary-nav" role="navigation">
 	<?php do_action( 'responsive_before_mega_nav', '' ); ?>
 	<div class="search-bar"><?php responsive_search_form(); ?></div>
-	<?php responsive_short_nav(); ?>
 	<?php responsive_primary_nav(); ?>
 	<div class="utility-down"><?php responsive_utility_nav(); ?></div>
 	<?php do_action( 'responsive_after_mega_nav', '' ); ?>
