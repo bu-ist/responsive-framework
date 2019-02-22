82303f2558c4f0d501c6378747bd76fa85c7f3b1
Todd Milliken <milliktr@gmail.com>
2019-02-22 12:15:46 -0500

    Move footer branding to a footer template part
    This change allows for child themes to provide their own overrides for just part of the footer, rather than needing to override the entire footer.

--- footer.php
+++ footer.php
@@ -59,29 +59,7 @@
 	?>
 
 	<footer class="site-footer <?php responsive_extra_footer_classes(); ?>" role="contentinfo">
-		<?php
-			/**
-			 * Fires immediately before the footer brand assets.
-			 *
-			 * @since 2.0.0
-			 */
-			do_action( 'r_before_footer_brand_assets' );
-		?>
-		<div class="site-footer-brand-assets">
-			<?php responsive_branding_masterplate(); ?>
-			<?php responsive_branding_bumc_logo(); ?>
-			<?php responsive_branding_disclaimer(); ?>
-			<?php responsive_customizer_footer_info(); ?>
-		</div>
-		<?php
-			/**
-			 * Fires immediately after the footer brand assets.
-			 *
-			 * @since 2.0.0
-			 */
-			do_action( 'r_after_footer_brand_assets' );
-		?>
-
+		<?php get_template_part( 'template-parts/footer-branding' ); ?>
 		<?php
 			/**
 			 * Fires immediately before the footer menus.
