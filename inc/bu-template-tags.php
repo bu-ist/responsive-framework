6bd86ad410600d49001986ef226ffe659bc0e21a
Todd Milliken <milliktr@gmail.com>
2019-03-01 09:58:08 -0500

    Bugfix on bu-navigation primarynav args
    The settings screen in BU Navigation no longer works because we are overwriting it` with `depth` and `max_items` args.
    
    Really, the only arguments we should be passing in is ‘primary-nav-menu’ for both `container_id` and `container_class`.
    
    BU Navigation provides default arguments here https://github.com/bu-ist/bu-navigation/blob/1.2.15/includes/library.php#L969-L984. Then it allows those defaults to be changed via the filter right below where it defines the default args: n/blob/1.2.15/includes/library.php#L985 . The plugin uses this filter here https://github.com/bu-ist/bu-navigation/blob/master/includes/settings.php#L24-L25to merge settings from the admin with the defaults, then allows anyone to override that result by passing in their own arguments when calling `bu_navigation_display_primary`.
    
    Since Framework provided all the defaults again, after it had already parsed bu-navigation defaults with the primary-navigation settings from the admin, we end up with a settings screen that doesn’t have any effect; the settings save but the front-end is hardcoded at 1 depth and 6 max items.
    
    The workaround here is to only pass in arguments that should override the plugin + plugin settings. In this case, its just the `container_id` and `container_class` args we need to supply to the function.
    
    One alternative to this original solution of passing the args into the function is to use the filter mentioned above at a higher priority that would fire first, https://github.com/bu-ist/bu-navigation/blob/1.2.15/includes/library.php#L985. This would filter the defaults in a similar fashion, as opposed to overriding the resulting args from plugin defaults + plugin admin settings.

--- inc/bu-template-tags.php
+++ inc/bu-template-tags.php
@@ -21,18 +21,8 @@ function responsive_primary_nav() {
 			 * Filters the BU navigation defaults.
 			 */
 			$args = apply_filters( 'bu_responsive_primary_nav_args', array(
-				'post_types'      => array( 'page' ),
-				'include_links'   => true,
-				'depth'           => BU_NAVIGATION_PRIMARY_DEPTH,
-				'max_items'       => BU_NAVIGATION_PRIMARY_MAX,
-				'container_tag'   => 'ul',
 				'container_id'    => 'primary-nav-menu',
 				'container_class' => 'primary-nav-menu',
-				'item_tag'        => 'li',
-				'identify_top'    => false,
-				'whitelist_top'   => null,
-				'title_before'    => '',
-				'title_after'     => '',
 			) );
 
 			bu_navigation_display_primary( $args );
