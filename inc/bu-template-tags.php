61c27e339504b55c3e45dbc53cbe92b8f2e5641a
Todd Milliken <milliktr@gmail.com>
2019-03-01 09:59:15 -0500

    phpcbf to autofix formatting based on WPCS 2.0

--- inc/bu-template-tags.php
+++ inc/bu-template-tags.php
@@ -11,7 +11,7 @@
  * BU custom version.
  *
  * @param string $nav Default HTML markup for the navigation.
- * @param array $args Default Responsive Framework nav arguments.
+ * @param array  $args Default Responsive Framework nav arguments.
  */
 function responsive_primary_nav() {
 	if ( ! method_exists( 'BuAccessControlPlugin', 'is_site_403' ) || false === BuAccessControlPlugin::is_site_403() ) {
@@ -20,10 +20,13 @@ function responsive_primary_nav() {
 			/**
 			 * Filters the BU navigation defaults.
 			 */
-			$args = apply_filters( 'bu_responsive_primary_nav_args', array(
-				'container_id'    => 'primary-nav-menu',
-				'container_class' => 'primary-nav-menu',
-			) );
+			$args = apply_filters(
+				'bu_responsive_primary_nav_args',
+				array(
+					'container_id'    => 'primary-nav-menu',
+					'container_class' => 'primary-nav-menu',
+				)
+			);
 
 			bu_navigation_display_primary( $args );
 		}
