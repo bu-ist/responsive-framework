aed6a31e82e3a7ffbf41111d672cd64c088abc2b
ashleykolodziej <kolodz10@bu.edu>
2019-06-13 15:59:30 -0400

    Remove old color picker UI code

--- admin/theme-customizer.js
+++ admin/theme-customizer.js
@@ -6,68 +6,6 @@
  */
 
 ( function( api, $ ) {
-	var cssTemplate = wp.template( 'responsive-framework-color-scheme' ),
-		colorSchemes = responsiveColor.schemes,
-		colorRegions = _.keys( responsiveColor.regions ),
-		optionalRegions = responsiveColor.optional;
-
-	api.controlConstructor.select = api.Control.extend( {
-		ready: function() {
-			if ( 'burf_setting_color_scheme' === this.id ) {
-				this.setting.bind( 'change', function( value ) {
-
-					// Update color pickers when new scheme is selected
-					_.each( colorRegions, function ( setting, index ) {
-						api( 'burf_setting_custom_colors[' + setting + ']' ).set( colorSchemes[ value ].colors[ index ] );
-						api.control( 'burf_setting_custom_colors[' + setting + ']' ).container.find( '.color-picker-hex' )
-							.data( 'data-default-color', colorSchemes[ value ].colors[ index ] )
-							.wpColorPicker( 'defaultColor', colorSchemes[ value ].colors[ index ] );
-					});
-
-					// Reset active region toggles
-					_.each( colorSchemes[value].active, function ( value, key ) {
-						api( 'burf_setting_active_color_regions[' + key + ']' ).set( value );
-					} );
-				} );
-			}
-		}
-	} );
-
-	// Generate the CSS for the current Color Scheme.
-	function updateCSS() {
-		var scheme = api( 'burf_setting_color_scheme' )(), css,
-			colors = _.object( colorRegions, colorSchemes[ scheme ].colors );
-
-		// Merge in color scheme overrides.
-		_.each( colorRegions, function( setting ) {
-			colors[ setting ] = api( 'burf_setting_custom_colors[' + setting + ']' )();
-		});
-
-		// Merge in optional region states for template conditionals
-		colors.active = {};
-		_.each( _.keys( colorSchemes[scheme].active ), function ( setting ) {
-			colors.active[ setting ] = api( 'burf_setting_active_color_regions[' + setting + ']' )();
-		} );
-
-		css = cssTemplate( colors );
-
-		api.previewer.send( 'update-color-scheme-css', css );
-	}
-
-	// Update the CSS whenever a color setting is changed.
-	_.each( colorRegions, function( setting ) {
-		api( 'burf_setting_custom_colors[' + setting + ']', function( setting ) {
-			// TODO: Toggle visibility of associated color picker
-			setting.bind( updateCSS );
-		} );
-	} );
-
-	// Update the CSS whenever an optional region checkbox is toggled.
-	_.each( optionalRegions, function ( region ) {
-		api( 'burf_setting_active_color_regions[' + region + ']', function( setting ) {
-			setting.bind( updateCSS );
-		} );
-	} );
 
 	// Sync checkbox group values to hidden setting checkbox
 	$( document ).ready( function () {
