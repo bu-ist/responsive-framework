06d0969b5bd12c5507290ac76a693c46b9eeed26
Jeremy Felt <jeremy.felt@gmail.com>
2020-01-15 15:55:02 -0800

    Add comments to explain query placeholders

--- inc/upgrade.php
+++ inc/upgrade.php
@@ -214,8 +214,10 @@ function responsive_upgrade_2_0( $verbose = true ) {
 		)
 	);
 
+	// Extract array keys for reuse when generating the query.
 	$template_map_keys = array_keys( $template_map );
 
+	// Prepare the query by adding a %s placeholder for each key of the passed array.
 	$results = $wpdb->get_results(
 		$wpdb->prepare(
 			"SELECT post_id, meta_value FROM $wpdb->postmeta WHERE meta_key = '_wp_page_template' AND meta_value IN (" . substr( str_repeat( ',%s', count( $template_map_keys ) ), 1 ) . ")", // phpcs:ignore WordPress.DB.PreparedSQL.NotPrepared
