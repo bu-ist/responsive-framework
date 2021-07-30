5026c439a6a6f7d1af4c03f488baea1b06fee92e
Jim Reevior <jim@hirozed.com>
2021-07-30 15:45:45 -0400

    Squashed bugs, no more PHP errors

--- searchpage.php
+++ searchpage.php
@@ -2,12 +2,10 @@
 /**
  * Template file used to render a Search Results Index page.
  * Template Name: Search Page
-*/
+ */
 
-get_header(); ?>
+get_header();
 
-	<?php responsive_content_banner( 'pageWidth' ); ?>
+get_template_part( 'template-parts/content-search', 'search' );
 
-	<!--?php get_template_part( 'template-parts/content-search', 'search' ); ?-->
-
-<?php get_footer();
+get_footer();
