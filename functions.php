f235818148b140d72b717ec3cbeec2e7afe91964
Frankline Ako <134519512+roytfrank@users.noreply.github.com>
2023-07-18 16:46:08 -0400

    #308 tweak to simplify check

--- functions.php
+++ functions.php
@@ -780,9 +780,9 @@ add_action( 'r_after_footer_menus', 'responsive_branding_copyright' );
  */
 function r_add_page_slug_to_body_tag_classes( $classes ) {
 	$post = get_post();
-	$classes_type = is_array( $classes ) ? true : false;
+	$chk_classes = is_array( $classes ) ? $classes : [];
 
-	if ( $post && $classes_type && ! in_array( $post->post_name, $classes, true ) ) {
+	if ( $post && ! in_array( $post->post_name, $chk_classes, true ) ) {
 		array_push( $classes, $post->post_name );
 	}
 
